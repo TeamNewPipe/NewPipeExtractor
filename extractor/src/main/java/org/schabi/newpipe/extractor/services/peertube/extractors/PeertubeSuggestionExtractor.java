706d4271a612332f651c07c0ec71fa859d7b8987
Ritvik Saraf <13ritvik@gmail.com>
2018-10-12 12:45:09 +0530

    empty suggestions for search

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/peertube/extractors/PeertubeSuggestionExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/peertube/extractors/PeertubeSuggestionExtractor.java
@@ -1,6 +1,7 @@
 package org.schabi.newpipe.extractor.services.peertube.extractors;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import org.schabi.newpipe.extractor.SuggestionExtractor;
@@ -11,13 +12,11 @@ public class PeertubeSuggestionExtractor extends SuggestionExtractor{
 
     public PeertubeSuggestionExtractor(int serviceId, Localization localization) {
         super(serviceId, localization);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public List<String> suggestionList(String query) throws IOException, ExtractionException {
-        // TODO Auto-generated method stub
-        return null;
+        return Collections.emptyList();
     }
 
 }
