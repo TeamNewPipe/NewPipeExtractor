91b1efc97e904a2323559002580eb7d6d8576acb
Christian Schabesberger <chris.schabesberger@mailbox.org>
2018-11-10 10:50:13 +0100

    add documentation of StreamingService

--- extractor/src/main/java/org/schabi/newpipe/extractor/ServiceList.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/ServiceList.java
@@ -8,6 +8,24 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableList;
 
+/*
+ * Copyright (C) Christian Schabesberger 2018 <chris.schabesberger@mailbox.org>
+ * ServiceList.java is part of NewPipe.
+ *
+ * NewPipe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NewPipe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NewPipe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * A list of supported services.
  */
@@ -19,6 +37,10 @@ public final class ServiceList {
     public static final YoutubeService YouTube;
     public static final SoundcloudService SoundCloud;
 
+    /**
+     * When creating a new service, put this service in the end of this list,
+     * and give it the next free id.
+     */
     private static final List<StreamingService> SERVICES = unmodifiableList(
             asList(
                     YouTube = new YoutubeService(0),
