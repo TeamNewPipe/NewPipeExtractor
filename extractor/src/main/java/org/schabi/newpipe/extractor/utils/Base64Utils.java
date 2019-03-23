8152ccf4fa15816e5995b88451eec9b5e3d47482
Vishan Seru <vishanseru9@gmail.com>
2019-03-24 00:10:20 +0530

    Add comment explaining purpose of Base64Utils and make bytes array argument non null

--- extractor/src/main/java/org/schabi/newpipe/extractor/utils/Base64Utils.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/utils/Base64Utils.java
@@ -3,8 +3,22 @@ package org.schabi.newpipe.extractor.utils;
 import org.apache.commons.codec.Charsets;
 import org.apache.commons.codec.binary.Base64;
 
+import javax.annotation.Nonnull;
+
 public class Base64Utils {
-    public static String encodeBase64String(byte[] bytes) {
+    /*
+    Using Base64.encodeBase64String() throws a NoSuchMethodError when
+    using it on Android. The reason behind this seems to be that
+    Android already includes an older version of Apache Commons Codec
+    which does not have this method, as per below SO thread:
+
+    https://stackoverflow.com/questions/2047706/apache-commons-codec-with-android-could-not-find-method
+
+    Hence, encodeBase64 method has been used which is an older method.
+    Creating a String out of it is exactly what was being done in the
+    library too, so that has been pulled in here.
+     */
+    public static String encodeBase64String(@Nonnull byte[] bytes) {
         return new String(Base64.encodeBase64(bytes), Charsets.UTF_8);
     }
 }
