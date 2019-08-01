3b621a766276bc97d9cb0ba7237aad9a04ba1772
Stypox <stypox@pm.me>
2019-08-01 09:55:15 +0200

    Default value for failRatioTriggeringIgnore in NewPipeTestRunner
    It is >=1.0f so that it makes the runner never ignore any failed test, disabling the feature

--- extractor/src/test/java/org/schabi/newpipe/NewPipeTestRunnerOptions.java
+++ extractor/src/test/java/org/schabi/newpipe/NewPipeTestRunnerOptions.java
@@ -11,7 +11,11 @@ public @interface NewPipeTestRunnerOptions {
      * Above this ratio the Runner will ignore all failing tests, since
      * there probably is a problem with YouTube blocking robots. A warning
      * along with a stack trace are displayed anyway.
+     * <p>
+     * A value >= 1.0f means the same as "never ignore any failed test"
+     * A value <= 0.0 means the same as "ignore all failed tests"
+     *
      * @return the failedTests/notIgnoredTests ratio above which all failing tests are ignored
      */
-    float failRatioTriggeringIgnore();
+    float failRatioTriggeringIgnore() default 1.1f;
 }
