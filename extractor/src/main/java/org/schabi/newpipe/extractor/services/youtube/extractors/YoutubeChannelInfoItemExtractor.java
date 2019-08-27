d1cd341592718dc6735adedac34f0637a4f80610
Stypox <stypox@pm.me>
2019-08-27 13:56:19 +0200

    Change comment

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeChannelInfoItemExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeChannelInfoItemExtractor.java
@@ -68,9 +68,9 @@ public class YoutubeChannelInfoItemExtractor implements ChannelInfoItemExtractor
             }
         } catch(Exception ignored) {}
 
+        // fallback method for channels without "Subscribe" button (or just in case yt changes things)
+        // provides an url with "/user/NAME", inconsistent with stream and channel extractor: tests will fail
         try {
-            // fallback method just in case youtube changes things; it should never run and tests will fail
-            // provides an url with "/user/NAME", that is inconsistent with stream and channel extractor
             return el.select("a[class*=\"yt-uix-tile-link\"]").first()
                     .attr("abs:href");
         } catch (Exception e) {
