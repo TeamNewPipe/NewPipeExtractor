aea44ad9a49283860671fd6f3c4e56bc7671e437
Stypox <stypox@pm.me>
2019-11-03 14:37:45 +0100

    Subclass BlockJUnit4ClassRunner in NewPipeTestRunner
    This removes much of the code that was used to get and invoke methods. Now the class Runner is also able to run individual tests.

--- extractor/src/test/java/org/schabi/newpipe/NewPipeTestRunner.java
+++ extractor/src/test/java/org/schabi/newpipe/NewPipeTestRunner.java
@@ -1,74 +1,52 @@
 package org.schabi.newpipe;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.internal.AssumptionViolatedException;
 import org.junit.runner.Description;
-import org.junit.runner.Runner;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
-import org.junit.runners.model.TestTimedOutException;
+import org.junit.runners.model.Statement;
 import org.schabi.newpipe.extractor.exceptions.ReCaptchaException;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-public class NewPipeTestRunner extends Runner {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
-    private class TestException extends Exception {
-        TestException(Throwable throwable) {
-            super(throwable);
-        }
-    }
-
+public class NewPipeTestRunner extends BlockJUnit4ClassRunner {
     
     private class MethodResultCollector {
-        boolean ignoredByUser;
-        String ignoredByUserReason;
-        String methodName;
-        Throwable thrownException;
+        @Nullable String ignoredByUserReason;
+        Description description;
 
-        MethodResultCollector(Method method, Object testClassInstance) throws IllegalAccessException, ExecutionException, InterruptedException {
-            ignoredByUser = method.isAnnotationPresent(Ignore.class);
-            methodName = method.getName();
-            thrownException = null;
+        @Nullable AssumptionViolatedException failedAssumption;
+        @Nullable Throwable thrownException;
 
-            if (ignoredByUser) {
-                ignoredByUserReason = method.getAnnotation(Ignore.class).value();
-                return;
-            }
+        MethodResultCollector(Description description, @Nonnull String ignoredByUserReason) {
+            this.description = description;
+            this.ignoredByUserReason = ignoredByUserReason;
+        }
 
-            Test testAnnotation = method.getAnnotation(Test.class);
-            Class expectedThrowable = testAnnotation.expected();
-            long timeout = testAnnotation.timeout();
+        MethodResultCollector(Description description, Statement statement) {
+            this.description = description;
+            this.ignoredByUserReason = null; // not ignored
 
             try {
-                invokeAllMethods(beforeMethods, testClassInstance);
-                invokeCheckingTimeAndExceptions(method, testClassInstance, timeout, expectedThrowable);
-                invokeAllMethods(afterMethods, testClassInstance);
-            } catch (InvocationTargetException e) {
-                thrownException = e.getCause();
-            } catch (TestException e) {
-                thrownException = e.getCause();
+                statement.evaluate();
+            } catch (AssumptionViolatedException e) {
+                failedAssumption = e;
+            } catch (Throwable e) {
+                thrownException = e;
             }
         }
 
 
         boolean isIgnoredByUser() {
-            return ignoredByUser;
+            return ignoredByUserReason != null;
         }
 
         boolean isSuccessful() {
@@ -77,191 +55,50 @@ public class NewPipeTestRunner extends Runner {
 
 
         void showResults(RunNotifier notifier, boolean ignoreAnyError) {
-            Description methodDescription = Description.createTestDescription(testClass, methodName);
-
-            if (ignoredByUser) {
-                notifier.fireTestIgnored(methodDescription);
-                if (ignoredByUserReason.isEmpty()) {
-                    System.out.println(methodName + "() ignored because of @Ignore");
-                } else {
-                    System.out.println(methodName + "() ignored because of @Ignore: " + ignoredByUserReason);
-                }
-
-            } else {
-                if (thrownException == null) {
-                    notifier.fireTestStarted(methodDescription);
-                    notifier.fireTestFinished(methodDescription);
+            if (ignoredByUserReason == null) { // not ignored
+                if (thrownException == null && failedAssumption == null) {
+                    notifier.fireTestStarted(description);
+                    notifier.fireTestFinished(description);
 
                 } else if (thrownException instanceof ReCaptchaException) {
-                    notifier.fireTestIgnored(methodDescription);
-                    System.out.println(methodName + "() ignored since it threw a ReCaptchaException");
+                    notifier.fireTestIgnored(description);
+                    System.out.println(description.getMethodName() + "() ignored since it threw a ReCaptchaException");
                     thrownException.printStackTrace(System.out);
 
                 } else if (ignoreAnyError) {
-                    notifier.fireTestIgnored(methodDescription);
-                    System.out.println(methodName + "() ignored since the whole class " + testClass.getName() +
+                    notifier.fireTestIgnored(description);
+                    System.out.println(description.getMethodName() + "() ignored since the whole class " + testClass.getName() +
                             " has more than " + (int)(failRatioTriggeringIgnore*100) + "% of failed tests");
                     thrownException.printStackTrace(System.out);
 
                 } else {
-                    notifier.fireTestStarted(methodDescription);
-                    notifier.fireTestFailure(new Failure(methodDescription, thrownException));
-                    notifier.fireTestFinished(methodDescription);
-
+                    notifier.fireTestStarted(description);
+                    if (thrownException == null) {
+                        notifier.fireTestAssumptionFailed(new Failure(description, failedAssumption));
+                    } else {
+                        notifier.fireTestFailure(new Failure(description, thrownException));
+                    }
+                    notifier.fireTestFinished(description);
                 }
+            } else {
+                notifier.fireTestIgnored(description);
+                System.out.println(description.getMethodName() + "() ignored because of @Ignore" +
+                        (ignoredByUserReason.isEmpty() ? "" : ": " + ignoredByUserReason));
             }
         }
 
     }
 
 
-    private Class testClass;
+    private final Class testClass;
     private float failRatioTriggeringIgnore;
-    private final ArrayList<Method> testMethods,
-            beforeMethods, afterMethods,
-            beforeClassMethods, afterClassMethods;
+    private List<MethodResultCollector> results;
 
     public NewPipeTestRunner(Class testClass) throws InitializationError {
+        super(testClass);
         this.testClass = testClass;
         obtainRunnerOptions();
-
-        testMethods = new ArrayList<>();
-        beforeMethods = new ArrayList<>();
-        afterMethods = new ArrayList<>();
-        beforeClassMethods = new ArrayList<>();
-        afterClassMethods = new ArrayList<>();
-
-        for (Method method : testClass.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(Test.class)) {
-                validatePublicVoidNoArg(method, false);
-                testMethods.add(method);
-
-            } else if (method.isAnnotationPresent(Before.class)) {
-                validatePublicVoidNoArg(method, false);
-                beforeMethods.add(method);
-
-            } else if (method.isAnnotationPresent(After.class)) {
-                validatePublicVoidNoArg(method, false);
-                afterMethods.add(method);
-
-            } else if (method.isAnnotationPresent(BeforeClass.class)) {
-                validatePublicVoidNoArg(method, true);
-                beforeClassMethods.add(method);
-
-            } else if (method.isAnnotationPresent(AfterClass.class)) {
-                validatePublicVoidNoArg(method, true);
-                afterClassMethods.add(method);
-
-            }
-        }
     }
-
-
-    @Override
-    public Description getDescription() {
-        return Description.createTestDescription(testClass, "NewPipe custom runner");
-    }
-
-
-    private static void validatePublicVoidNoArg(Method method, boolean shouldBeStatic) throws InitializationError {
-        List<Throwable> errors = new ArrayList<>();
-
-        if (method.getParameterTypes().length != 0) {
-            errors.add(new Exception("Method " + method.getName() + " should have no parameters"));
-        }
-        if (Modifier.isStatic(method.getModifiers()) != shouldBeStatic) {
-            String state = shouldBeStatic ? "should" : "should not";
-            errors.add(new Exception("Method " + method.getName() + "() " + state + " be static"));
-        }
-        if (!Modifier.isPublic(method.getModifiers())) {
-            errors.add(new Exception("Method " + method.getName() + "() should be public"));
-        }
-        if (method.getReturnType() != Void.TYPE) {
-            errors.add(new Exception("Method " + method.getName() + "() should be void"));
-        }
-
-        if (!errors.isEmpty()) {
-            throw new InitializationError(errors);
-        }
-    }
-
-    private static void invokeAllMethods(List<Method> methods, Object testClassInstance) throws InvocationTargetException, IllegalAccessException {
-        for (Method method : methods) {
-            method.invoke(testClassInstance);
-        }
-    }
-
-    class MethodRunnableCollectingExceptions implements Runnable {
-        InvocationTargetException collectedInvocationTargetException;
-        IllegalAccessException collectedIllegalAccessException;
-        Method method;
-        Object testClassInstance;
-
-        MethodRunnableCollectingExceptions(Method method, Object testClassInstance) {
-            this.method = method;
-            this.testClassInstance = testClassInstance;
-        }
-
-        @Override
-        public final void run() {
-            try {
-                method.invoke(testClassInstance);
-            } catch (InvocationTargetException e) {
-                collectedInvocationTargetException = e;
-            } catch (IllegalAccessException e) {
-                collectedIllegalAccessException = e;
-            }
-        }
-
-        void throwCollectedThrowableIfPresent() throws InvocationTargetException, IllegalAccessException {
-            if (collectedInvocationTargetException != null) {
-                throw collectedInvocationTargetException;
-            } else if (collectedIllegalAccessException != null) {
-                throw collectedIllegalAccessException;
-            }
-        }
-    }
-
-    private void invokeWithTimeout(final Method method, final Object testClassInstance, long timeout) throws InvocationTargetException, IllegalAccessException, TestException, InterruptedException, ExecutionException {
-        if (timeout > 0) {
-            final ExecutorService executorService = Executors.newSingleThreadExecutor();
-            MethodRunnableCollectingExceptions runnable = new MethodRunnableCollectingExceptions(method, testClassInstance);
-
-            try {
-                final Future<Object> f = (Future<Object>) executorService.submit(runnable);
-                f.get(timeout, TimeUnit.MILLISECONDS);
-                runnable.throwCollectedThrowableIfPresent();
-            } catch (final TimeoutException e) {
-                throw new TestException(new TestTimedOutException(timeout, TimeUnit.MILLISECONDS));
-            } finally {
-                executorService.shutdown();
-            }
-        } else {
-            method.invoke(testClassInstance);
-        }
-    }
-
-    private void invokeCheckingTimeAndExceptions(Method method, Object testClassInstance, long timeout, Class expectedThrowable) throws InvocationTargetException, IllegalAccessException, TestException, ExecutionException, InterruptedException {
-        boolean complete = false;
-        try {
-            invokeWithTimeout(method, testClassInstance, timeout);
-            complete = true;
-        } catch (InvocationTargetException e) {
-            if (expectedThrowable.equals(Test.None.class)) {
-                throw e;
-            } else if (!expectedThrowable.isInstance(e.getCause())) {
-                String message = "Unexpected exception, expected <" + expectedThrowable.getName() +
-                        "> but was <" + e.getCause().getClass().getName() + ">";
-                throw new TestException(new Exception(message, e));
-            }
-        }
-
-        if (complete && !expectedThrowable.equals(Test.None.class)) {
-            String message = "Expected exception <" + expectedThrowable.getName() + ">";
-            throw new TestException(new Exception(message));
-        }
-    }
-
 
     private void obtainRunnerOptions() {
         if (testClass.isAnnotationPresent(NewPipeTestRunnerOptions.class)) {
@@ -276,33 +113,39 @@ public class NewPipeTestRunner extends Runner {
 
     @Override
     public void run(RunNotifier notifier) {
-        try {
-            Object testClassInstance = testClass.newInstance();
-            invokeAllMethods(beforeClassMethods, testClassInstance);
+        results = new ArrayList<>();
+        super.run(notifier);
 
-            List<MethodResultCollector> savedResults = new ArrayList<>();
-            for (Method testMethod : testMethods) {
-                savedResults.add(new MethodResultCollector(testMethod, testClassInstance));
-            }
-
-            int nrSuccessful = 0, nrNotIgnoredByUser = 0;
-            for (MethodResultCollector savedResult : savedResults) {
-                if (!savedResult.isIgnoredByUser()) {
-                    ++nrNotIgnoredByUser;
-                    if (savedResult.isSuccessful()) {
-                        ++nrSuccessful;
-                    }
+        int nrSuccessful = 0, nrNotIgnoredByUser = 0;
+        for (MethodResultCollector result : results) {
+            if (!result.isIgnoredByUser()) {
+                ++nrNotIgnoredByUser;
+                if (result.isSuccessful()) {
+                    ++nrSuccessful;
                 }
             }
+        }
 
-            for (MethodResultCollector savedResult : savedResults) {
-                savedResult.showResults(notifier, (float)(nrNotIgnoredByUser - nrSuccessful) / nrNotIgnoredByUser > failRatioTriggeringIgnore);
-            }
+        boolean ignoreAnyError;
+        if (nrNotIgnoredByUser <= 1) {
+            ignoreAnyError = false;
+        } else {
+            ignoreAnyError = ((float)(nrNotIgnoredByUser - nrSuccessful) / nrNotIgnoredByUser) > failRatioTriggeringIgnore;
+        }
 
-            invokeAllMethods(afterClassMethods, testClassInstance);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        for (MethodResultCollector result : results) {
+            result.showResults(notifier, ignoreAnyError);
         }
     }
 
+    @Override
+    protected void runChild(FrameworkMethod method, RunNotifier notifier) {
+        Description description = describeChild(method);
+        if (isIgnored(method)) {
+            results.add(new MethodResultCollector(description, method.getAnnotation(Ignore.class).value()));
+        } else {
+            Statement statement = methodBlock(method);
+            results.add(new MethodResultCollector(description, statement));
+        }
+    }
 }
