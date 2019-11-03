cdc6c21d0e4965bcccff0a381cb8e86f98a0c33e
Stypox <stypox@pm.me>
2019-11-03 15:43:00 +0100

    Remove failRatioTriggeringIgnore
    It is too risky to silently ignore tests that actually failed. The fact that many tests in a class fail indicates that there is a problem in the set-up methods (`@BeforeClass`). We can't assume that the problem in set-up methods is a network error, since it could as well be a programming error.
    
    Also `ReCaptchaException`s are now considered a violated assumption (same as AssumptionViolatedException), since that's what they are. This allows to call `fireTestStarted` before invoking the tested method, thus correctly mesuring the elapsed time.

--- extractor/src/test/java/org/schabi/newpipe/NewPipeTestRunner.java
+++ extractor/src/test/java/org/schabi/newpipe/NewPipeTestRunner.java
@@ -11,141 +11,45 @@ import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.Statement;
 import org.schabi.newpipe.extractor.exceptions.ReCaptchaException;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 public class NewPipeTestRunner extends BlockJUnit4ClassRunner {
-    
-    private class MethodResultCollector {
-        @Nullable String ignoredByUserReason;
-        Description description;
-
-        @Nullable AssumptionViolatedException failedAssumption;
-        @Nullable Throwable thrownException;
-
-        MethodResultCollector(Description description, @Nonnull String ignoredByUserReason) {
-            this.description = description;
-            this.ignoredByUserReason = ignoredByUserReason;
-        }
-
-        MethodResultCollector(Description description, Statement statement) {
-            this.description = description;
-            this.ignoredByUserReason = null; // not ignored
-
-            try {
-                statement.evaluate();
-            } catch (AssumptionViolatedException e) {
-                failedAssumption = e;
-            } catch (Throwable e) {
-                thrownException = e;
-            }
-        }
-
-
-        boolean isIgnoredByUser() {
-            return ignoredByUserReason != null;
-        }
-
-        boolean isSuccessful() {
-            return thrownException == null;
-        }
-
-
-        void showResults(RunNotifier notifier, boolean ignoreAnyError) {
-            if (ignoredByUserReason == null) { // not ignored
-                if (thrownException == null && failedAssumption == null) {
-                    notifier.fireTestStarted(description);
-                    notifier.fireTestFinished(description);
-
-                } else if (thrownException instanceof ReCaptchaException) {
-                    notifier.fireTestIgnored(description);
-                    System.out.println(description.getMethodName() + "() ignored since it threw a ReCaptchaException");
-                    thrownException.printStackTrace(System.out);
-
-                } else if (ignoreAnyError) {
-                    notifier.fireTestIgnored(description);
-                    System.out.println(description.getMethodName() + "() ignored since the whole class " + testClass.getName() +
-                            " has more than " + (int)(failRatioTriggeringIgnore*100) + "% of failed tests");
-                    thrownException.printStackTrace(System.out);
-
-                } else {
-                    notifier.fireTestStarted(description);
-                    if (thrownException == null) {
-                        notifier.fireTestAssumptionFailed(new Failure(description, failedAssumption));
-                    } else {
-                        notifier.fireTestFailure(new Failure(description, thrownException));
-                    }
-                    notifier.fireTestFinished(description);
-                }
-            } else {
-                notifier.fireTestIgnored(description);
-                System.out.println(description.getMethodName() + "() ignored because of @Ignore" +
-                        (ignoredByUserReason.isEmpty() ? "" : ": " + ignoredByUserReason));
-            }
-        }
-
-    }
-
-
-    private final Class testClass;
-    private float failRatioTriggeringIgnore;
-    private List<MethodResultCollector> results;
-
     public NewPipeTestRunner(Class testClass) throws InitializationError {
         super(testClass);
-        this.testClass = testClass;
-        obtainRunnerOptions();
-    }
 
-    private void obtainRunnerOptions() {
         if (testClass.isAnnotationPresent(NewPipeTestRunnerOptions.class)) {
             NewPipeTestRunnerOptions options = (NewPipeTestRunnerOptions) testClass.getAnnotation(NewPipeTestRunnerOptions.class);
-
-            failRatioTriggeringIgnore = options.failRatioTriggeringIgnore();
         } else {
             throw new IllegalArgumentException("Test classes running with " + NewPipeTestRunner.class.getName() + " should also have @NewPipeTestRunnerOptions");
-        }
-    }
-
-
-    @Override
-    public void run(RunNotifier notifier) {
-        results = new ArrayList<>();
-        super.run(notifier);
-
-        int nrSuccessful = 0, nrNotIgnoredByUser = 0;
-        for (MethodResultCollector result : results) {
-            if (!result.isIgnoredByUser()) {
-                ++nrNotIgnoredByUser;
-                if (result.isSuccessful()) {
-                    ++nrSuccessful;
-                }
-            }
-        }
-
-        boolean ignoreAnyError;
-        if (nrNotIgnoredByUser <= 1) {
-            ignoreAnyError = false;
-        } else {
-            ignoreAnyError = ((float)(nrNotIgnoredByUser - nrSuccessful) / nrNotIgnoredByUser) > failRatioTriggeringIgnore;
-        }
-
-        for (MethodResultCollector result : results) {
-            result.showResults(notifier, ignoreAnyError);
         }
     }
 
     @Override
     protected void runChild(FrameworkMethod method, RunNotifier notifier) {
         Description description = describeChild(method);
+
         if (isIgnored(method)) {
-            results.add(new MethodResultCollector(description, method.getAnnotation(Ignore.class).value()));
+            notifier.fireTestIgnored(description);
+
+            String ignoreReason = method.getAnnotation(Ignore.class).value();
+            System.out.println(method.getName() + "() ignored because of @Ignore" +
+                    (ignoreReason.isEmpty() ? "" : ": " + ignoreReason));
         } else {
             Statement statement = methodBlock(method);
-            results.add(new MethodResultCollector(description, statement));
+
+            notifier.fireTestStarted(description);
+            try {
+                statement.evaluate();
+            } catch (AssumptionViolatedException | ReCaptchaException e) {
+                notifier.fireTestAssumptionFailed(new Failure(description, e));
+
+                if (e instanceof ReCaptchaException) {
+                    System.out.println(method.getName() + "() ignored since it threw a ReCaptchaException");
+                }
+
+            } catch (Throwable e) {
+                notifier.fireTestFailure(new Failure(description, e));
+            } finally {
+                notifier.fireTestFinished(description);
+            }
         }
     }
 }
--- extractor/src/test/java/org/schabi/newpipe/NewPipeTestRunnerOptions.java
+++ extractor/src/test/java/org/schabi/newpipe/NewPipeTestRunnerOptions.java
@@ -5,17 +5,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NewPipeTestRunnerOptions {
-    /**
-     * The ratio between the number of failed tests and the total number
-     * of tests that have not been {@link org.junit.Ignore}d by the user.
-     * Above this ratio the Runner will ignore all failing tests, since
-     * there probably is a problem with YouTube blocking robots. A warning
-     * along with a stack trace are displayed anyway.
-     * <p>
-     * A value >= 1.0f means the same as "never ignore any failed test"
-     * A value <= 0.0 means the same as "ignore all failed tests"
-     *
-     * @return the failedTests/notIgnoredTests ratio above which all failing tests are ignored
-     */
-    float failRatioTriggeringIgnore() default 1.1f;
+
 }
