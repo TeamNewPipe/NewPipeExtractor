8225a4757e34118cd41569866fd5a5873b03ecba
Tim <fbfbfbtom@gmail.com>
2019-11-17 20:12:17 +0800

    Avoid using X in SimpleDateFormat

--- extractor/src/main/java/org/schabi/newpipe/extractor/utils/DateUtils.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/utils/DateUtils.java
@@ -7,15 +7,27 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.*;
 
 public class DateUtils {
-    private static Date tryParseDateTimeString(String dateTime) throws ParsingException {
+    private static Date tryParseDateTimeString(String rawStr) throws ParsingException {
+        Pattern pattern = Pattern.compile("^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}[+-]\\d{2}):(\\d{2})$");
+        Matcher m = pattern.matcher(rawStr);
+        String dateTime = rawStr;
+
+        // Since we need API 24+ to match timezone with colon in SimpleDateFormat,
+        // we remove colon from the timezone if there is any
+        // https://developer.android.com/reference/java/text/SimpleDateFormat
+        if (m.find()) {
+            dateTime = m.group(1) + m.group(2);
+        }
+
         // Since SimpleDateFormat::parse is not thread-safe, we create new SimpleDateFormat instances here
         List<SimpleDateFormat> possibleDateTimeFormats = Arrays.asList(
             new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+00:00"),    // Youtube RSS feed
             new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'"),       // SoundCloud API
             new SimpleDateFormat("yyyy/MM/dd HH:mm:ss +0000"),      // SoundCloud API
-            new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")    // MediaCCC API
+            new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")      // MediaCCC API
         );
 
         ParseException ex = null;
