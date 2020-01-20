f403490bd9ada2b19176ffc694d5b5ad405975b6
B0pol <bopol@e.email>
2020-01-20 21:28:26 +0100

    Refactoring
    Co-Authored-By: Tobias Groza <TobiGr@users.noreply.github.com>

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/peertube/extractors/PeertubeStreamExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/peertube/extractors/PeertubeStreamExtractor.java
@@ -76,7 +76,7 @@ public class PeertubeStreamExtractor extends StreamExtractor {
         } catch (ParsingException e) {
             return "No description";
         }
-        if (desc.length() == 250 && desc.substring(desc.length() - 3).equals("...")) {
+        if (desc.length() == 250 && desc.substring(247).equals("...")) {
             //if description is shortened, get full description
             Downloader dl = NewPipe.getDownloader();
             try {
