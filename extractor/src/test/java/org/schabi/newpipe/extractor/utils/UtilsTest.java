e52a86d0ea375f402226e39adcbd5e19753002b4
bopol <bopol@e.email>
2020-02-19 15:28:47 +0100

    improve createFile func

--- extractor/src/test/java/org/schabi/newpipe/extractor/utils/UtilsTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/utils/UtilsTest.java
@@ -1,12 +1,17 @@
 package org.schabi.newpipe.extractor.utils;
 
+import com.grack.nanojson.JsonObject;
 import com.grack.nanojson.JsonParserException;
+import com.grack.nanojson.JsonWriter;
 import org.junit.Test;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,8 +26,29 @@ public class UtilsTest {
     }
 
     public static void createFile(String path, String content) throws IOException {
+        String[] dirs = path.split("/");
+        if (dirs.length > 1) {
+            String pathWithoutFileName = path.replace(dirs[dirs.length - 1], "");
+            if (!Files.exists(Paths.get(pathWithoutFileName))) { //create dirs if they don't exist
+                new File(pathWithoutFileName).mkdirs();
+            }
+        }
+        writeFile(path, content);
+    }
+
+    //lower lever createFile. Doesn't create directories and takes only a String
+    public static void writeFile(String path, String content) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(path));
         writer.write(content);
+        writer.flush();
         writer.close();
+    }
+
+    public static String jsonObjToString(JsonObject object) {
+        return JsonWriter.string(object);
+    }
+
+    public static void createFile(String path, JsonObject content) throws IOException {
+        createFile(path, jsonObjToString(content));
     }
 }
