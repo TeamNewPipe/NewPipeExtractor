beb07a4852fc27c65ff2db56270085dd663a8012
wb9688 <wb9688@users.noreply.github.com>
2020-02-28 16:36:14 +0100

    Add test to check whether hardcoded client version is still valid

---
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/YoutubeParsingHelperTest.java
@@ -0,0 +1,22 @@
+package org.schabi.newpipe.extractor.services.youtube;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.schabi.newpipe.DownloaderTestImpl;
+import org.schabi.newpipe.extractor.NewPipe;
+import org.schabi.newpipe.extractor.services.youtube.linkHandler.YoutubeParsingHelper;
+
+import static org.junit.Assert.assertTrue;
+
+public class YoutubeParsingHelperTest {
+    @BeforeClass
+    public static void setUp() {
+        NewPipe.init(DownloaderTestImpl.getInstance());
+    }
+
+    @Test
+    public void testIsHardcodedClientVersionValid() {
+        assertTrue("Hardcoded client version is not valid anymore",
+                YoutubeParsingHelper.isHardcodedClientVersionValid());
+    }
+}
