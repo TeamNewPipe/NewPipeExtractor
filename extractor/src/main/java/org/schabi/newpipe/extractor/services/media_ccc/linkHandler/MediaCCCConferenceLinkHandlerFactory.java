07a90d116a0059a027f158f59b5fe9d2720f361f
Stypox <stypox@pm.me>
2020-04-11 16:57:31 +0200

    [MediaCCC] Use regex to parse stream and conference urls

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/linkHandler/MediaCCCConferenceLinkHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/linkHandler/MediaCCCConferenceLinkHandlerFactory.java
@@ -9,32 +9,24 @@ import java.util.List;
 public class MediaCCCConferenceLinkHandlerFactory extends ListLinkHandlerFactory {
     public static final String CONFERENCE_API_ENDPOINT = "https://api.media.ccc.de/public/conferences/";
     public static final String CONFERENCE_PATH = "https://media.ccc.de/c/";
+    private static final String ID_PATTERN = "(?:(?:(?:api\\.)?media\\.ccc\\.de/public/conferences/)|(?:media\\.ccc\\.de/[bc]/))([^/?&#]*)";
 
     @Override
-    public String getUrl(final String id, final List<String> contentFilter,
+    public String getUrl(final String id,
+                         final List<String> contentFilter,
                          final String sortFilter) throws ParsingException {
         return CONFERENCE_PATH + id;
     }
 
     @Override
     public String getId(final String url) throws ParsingException {
-        if (url.startsWith(CONFERENCE_API_ENDPOINT)) {
-            return url.replace(CONFERENCE_API_ENDPOINT, "");
-        } else if (url.startsWith("https://media.ccc.de/public/conferences/")) {
-            return url.replace("https://media.ccc.de/public/conferences/", "");
-        } else if (url.startsWith(CONFERENCE_PATH)) {
-            return Parser.matchGroup1(CONFERENCE_PATH + "([^?#]*)", url);
-        } else if (url.startsWith("https://media.ccc.de/b/")) {
-            return Parser.matchGroup1("https://media.ccc.de/b/([^?#]*)", url);
-        }
-        throw new ParsingException("Could not get id from url: " + url);
+        return Parser.matchGroup1(ID_PATTERN, url);
     }
 
     @Override
     public boolean onAcceptUrl(final String url) {
         try {
-            getId(url);
-            return true;
+            return getId(url) != null;
         } catch (ParsingException e) {
             return false;
         }
