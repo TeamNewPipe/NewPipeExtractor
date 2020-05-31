3239aa84f237d2af07a4d586bd282b335c188716
wb9688 <wb9688@users.noreply.github.com>
2020-05-31 13:33:14 +0200

    Handle isDeleted for PeerTube comments

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/peertube/PeertubeCommentsExtractorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/peertube/PeertubeCommentsExtractorTest.java
@@ -18,74 +18,97 @@ import static org.junit.Assert.*;
 import static org.schabi.newpipe.extractor.ServiceList.PeerTube;
 
 public class PeertubeCommentsExtractorTest {
+    public static class Default {
+        private static PeertubeCommentsExtractor extractor;
 
-    private static PeertubeCommentsExtractor extractor;
-
-    @BeforeClass
-    public static void setUp() throws Exception {
-        NewPipe.init(DownloaderTestImpl.getInstance());
-        extractor = (PeertubeCommentsExtractor) PeerTube
-                .getCommentsExtractor("https://framatube.org/videos/watch/04af977f-4201-4697-be67-a8d8cae6fa7a");
-    }
-
-    @Test
-    public void testGetComments() throws IOException, ExtractionException {
-        boolean result = false;
-        InfoItemsPage<CommentsInfoItem> comments = extractor.getInitialPage();
-        result = findInComments(comments, "@root A great documentary on a great guy.");
-
-        while (comments.hasNextPage() && !result) {
-            comments = extractor.getPage(comments.getNextPageUrl());
-            result = findInComments(comments, "@root A great documentary on a great guy.");
+        @BeforeClass
+        public static void setUp() throws Exception {
+            NewPipe.init(DownloaderTestImpl.getInstance());
+            extractor = (PeertubeCommentsExtractor) PeerTube
+                    .getCommentsExtractor("https://framatube.org/videos/watch/04af977f-4201-4697-be67-a8d8cae6fa7a");
         }
 
-        assertTrue(result);
-    }
+        @Test
+        public void testGetComments() throws IOException, ExtractionException {
+            InfoItemsPage<CommentsInfoItem> comments = extractor.getInitialPage();
+            assertTrue(comments.getErrors().isEmpty());
 
-    @Test
-    public void testGetCommentsFromCommentsInfo() throws IOException, ExtractionException {
-        boolean result = false;
-        CommentsInfo commentsInfo = CommentsInfo.getInfo("https://framatube.org/videos/watch/a8ea95b8-0396-49a6-8f30-e25e25fb2828");
-        assertEquals("Comments", commentsInfo.getName());
-        result = findInComments(commentsInfo.getRelatedItems(), "Loved it!!!");
+            boolean result = findInComments(comments, "@root A great documentary on a great guy.");
+            while (comments.hasNextPage() && !result) {
+                comments = extractor.getPage(comments.getNextPageUrl());
+                result = findInComments(comments, "@root A great documentary on a great guy.");
+            }
 
-        String nextPage = commentsInfo.getNextPageUrl();
-        while (!Utils.isBlank(nextPage) && !result) {
-            InfoItemsPage<CommentsInfoItem> moreItems = CommentsInfo.getMoreItems(PeerTube, commentsInfo, nextPage);
-            result = findInComments(moreItems.getItems(), "Loved it!!!");
-            nextPage = moreItems.getNextPageUrl();
+            assertTrue(result);
         }
 
-        assertTrue(result);
-    }
+        @Test
+        public void testGetCommentsFromCommentsInfo() throws IOException, ExtractionException {
+            final CommentsInfo commentsInfo = CommentsInfo.getInfo("https://framatube.org/videos/watch/a8ea95b8-0396-49a6-8f30-e25e25fb2828");
+            assertTrue(commentsInfo.getErrors().isEmpty());
+            assertEquals("Comments", commentsInfo.getName());
 
-    @Test
-    public void testGetCommentsAllData() throws IOException, ExtractionException {
-        InfoItemsPage<CommentsInfoItem> comments = extractor.getInitialPage();
-        for (CommentsInfoItem c : comments.getItems()) {
-            assertFalse(Utils.isBlank(c.getUploaderUrl()));
-            assertFalse(Utils.isBlank(c.getUploaderName()));
-            assertFalse(Utils.isBlank(c.getUploaderAvatarUrl()));
-            assertFalse(Utils.isBlank(c.getCommentId()));
-            assertFalse(Utils.isBlank(c.getCommentText()));
-            assertFalse(Utils.isBlank(c.getName()));
-            assertFalse(Utils.isBlank(c.getTextualUploadDate()));
-            assertFalse(Utils.isBlank(c.getThumbnailUrl()));
-            assertFalse(Utils.isBlank(c.getUrl()));
-            assertFalse(c.getLikeCount() != -1);
+            boolean result = findInComments(commentsInfo.getRelatedItems(), "Loved it!!!");
+            String nextPage = commentsInfo.getNextPageUrl();
+            while (!Utils.isBlank(nextPage) && !result) {
+                final InfoItemsPage<CommentsInfoItem> moreItems = CommentsInfo.getMoreItems(PeerTube, commentsInfo, nextPage);
+                result = findInComments(moreItems.getItems(), "Loved it!!!");
+                nextPage = moreItems.getNextPageUrl();
+            }
+            assertTrue(result);
         }
-    }
 
-    private boolean findInComments(InfoItemsPage<CommentsInfoItem> comments, String comment) {
-        return findInComments(comments.getItems(), comment);
-    }
-
-    private boolean findInComments(List<CommentsInfoItem> comments, String comment) {
-        for (CommentsInfoItem c : comments) {
-            if (c.getCommentText().contains(comment)) {
-                return true;
+        @Test
+        public void testGetCommentsAllData() throws IOException, ExtractionException {
+            final InfoItemsPage<CommentsInfoItem> comments = extractor.getInitialPage();
+            for (final CommentsInfoItem c : comments.getItems()) {
+                assertFalse(Utils.isBlank(c.getUploaderUrl()));
+                assertFalse(Utils.isBlank(c.getUploaderName()));
+                assertFalse(Utils.isBlank(c.getUploaderAvatarUrl()));
+                assertFalse(Utils.isBlank(c.getCommentId()));
+                assertFalse(Utils.isBlank(c.getCommentText()));
+                assertFalse(Utils.isBlank(c.getName()));
+                assertFalse(Utils.isBlank(c.getTextualUploadDate()));
+                assertFalse(Utils.isBlank(c.getThumbnailUrl()));
+                assertFalse(Utils.isBlank(c.getUrl()));
+                assertEquals(-1, c.getLikeCount());
             }
         }
-        return false;
+
+        private boolean findInComments(final InfoItemsPage<CommentsInfoItem> comments, final String comment) {
+            return findInComments(comments.getItems(), comment);
+        }
+
+        private boolean findInComments(final List<CommentsInfoItem> comments, final String comment) {
+            for (final CommentsInfoItem c : comments) {
+                if (c.getCommentText().contains(comment)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static class DeletedComments {
+        private static PeertubeCommentsExtractor extractor;
+
+        @BeforeClass
+        public static void setUp() throws Exception {
+            NewPipe.init(DownloaderTestImpl.getInstance());
+            extractor = (PeertubeCommentsExtractor) PeerTube
+                    .getCommentsExtractor("https://framatube.org/videos/watch/217eefeb-883d-45be-b7fc-a788ad8507d3");
+        }
+
+        @Test
+        public void testGetComments() throws IOException, ExtractionException {
+            final InfoItemsPage<CommentsInfoItem> comments = extractor.getInitialPage();
+            assertTrue(comments.getErrors().isEmpty());
+        }
+
+        @Test
+        public void testGetCommentsFromCommentsInfo() throws IOException, ExtractionException {
+            final CommentsInfo commentsInfo = CommentsInfo.getInfo("https://framatube.org/videos/watch/217eefeb-883d-45be-b7fc-a788ad8507d3");
+            assertTrue(commentsInfo.getErrors().isEmpty());
+        }
     }
 }
