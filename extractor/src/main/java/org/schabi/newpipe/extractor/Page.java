4c987a530292bc4b5a43540efcdf4b262c19de0d
wb9688 <wb9688@users.noreply.github.com>
2020-07-26 10:01:03 +0200

    Support YouTube's new continuations for search

--- extractor/src/main/java/org/schabi/newpipe/extractor/Page.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/Page.java
@@ -8,33 +8,43 @@ import static org.schabi.newpipe.extractor.utils.Utils.isNullOrEmpty;
 
 public class Page implements Serializable {
     private final String url;
+    private final String id;
     private final List<String> ids;
     private final Map<String, String> cookies;
 
-    public Page(final String url, final List<String> ids, final Map<String, String> cookies) {
+    public Page(final String url, final String id, final List<String> ids, final Map<String, String> cookies) {
         this.url = url;
+        this.id = id;
         this.ids = ids;
         this.cookies = cookies;
     }
 
     public Page(final String url) {
-        this(url, null, null);
+        this(url, null, null, null);
+    }
+
+    public Page(final String url, final String id) {
+        this(url, id, null, null);
     }
 
     public Page(final String url, final Map<String, String> cookies) {
-        this(url, null, cookies);
+        this(url, null, null, cookies);
     }
 
     public Page(final List<String> ids) {
-        this(null, ids, null);
+        this(null, null, ids, null);
     }
 
     public Page(final List<String> ids, final Map<String, String> cookies) {
-        this(null, ids, cookies);
+        this(null, null, ids, cookies);
     }
 
     public String getUrl() {
         return url;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public List<String> getIds() {
