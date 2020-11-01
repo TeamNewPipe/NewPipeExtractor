82746d172f470dec6f88ff49c0815b8041d9f2d7
Bri@n <bdonaldson@pm.me>
2020-11-01 17:34:34 -0500

    Fix typo in DonationLinkHelper and rewrote swtich statement

--- extractor/src/main/java/org/schabi/newpipe/extractor/utils/DonationLinkHelper.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/utils/DonationLinkHelper.java
@@ -15,16 +15,14 @@ public class DonationLinkHelper {
         AMAZON,
     }
 
-    public static DonationService getDonatoinServiceByLink(String link) throws MalformedURLException {
+    public static DonationService getDonationServiceByLink(String link) throws MalformedURLException {
         URL url = new URL(fixLink(link));
         switch (url.getHost()) {
             case "www.patreon.com":
-                return DonationService.PATREON;
             case "patreon.com":
                 return DonationService.PATREON;
-            case "paypal.me":
-                return DonationService.PAYPAL;
             case "www.paypal.me":
+            case "paypal.me":
                 return DonationService.PAYPAL;
             default:
                 return DonationService.NO_DONATION;
