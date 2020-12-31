2cbc3ccb1afbd4693bd20d6017b3e2887c9441cb
TobiGr <tobigr@users.noreply.github.com>
2020-12-31 17:03:14 +0100

    [media.ccc.de] Fix live stream test if there are no current live streams

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/media_ccc/MediaCCCLiveStreamListExtractorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/media_ccc/MediaCCCLiveStreamListExtractorTest.java
@@ -28,7 +28,6 @@ public class MediaCCCLiveStreamListExtractorTest {
     @Test
     public void getConferencesListTest() throws Exception {
         final List<InfoItem> a = extractor.getInitialPage().getItems();
-        assertTrue(a.size() > 2);
         for (int i = 0; i < a.size(); i++) {
             final InfoItem b = a.get(i);
             assertNotNull(a.get(i).getName());
