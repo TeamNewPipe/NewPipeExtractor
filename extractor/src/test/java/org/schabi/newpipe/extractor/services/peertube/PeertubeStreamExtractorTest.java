34722a1b3ae7d734620adab31ec814b9986f0be6
TiA4f8R <74829229+TiA4f8R@users.noreply.github.com>
2021-08-07 17:44:43 +0200

    Change a message in PeertubeStreamExtractorTest
    The issue in this test is not a SSL problem anymore.

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/peertube/PeertubeStreamExtractorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/peertube/PeertubeStreamExtractorTest.java
@@ -140,7 +140,7 @@ public abstract class PeertubeStreamExtractorTest extends DefaultStreamExtractor
         @Override public List<String> expectedTags() { return Arrays.asList("Marinauts", "adobe flash", "adobe flash player", "flash games", "the marinauts"); }
     }
 
-    @Ignore("Test broken, SSL problem")
+    @Ignore("Test broken, instance doesn't exist anymore")
     public static class AgeRestricted extends PeertubeStreamExtractorTest {
         private static final String ID = "dbd8e5e1-c527-49b6-b70c-89101dbb9c08";
         private static final String INSTANCE = "https://nocensoring.net";
