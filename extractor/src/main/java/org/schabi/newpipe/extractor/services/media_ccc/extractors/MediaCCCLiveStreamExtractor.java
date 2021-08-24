642e4573824445aa6a397cdf431c8c55fbb53ee2
TiA4f8R <74829229+TiA4f8R@users.noreply.github.com>
2021-08-24 18:49:41 +0200

    [Media.ccc.de] Improve stream parsing of livestreams
    Remove a commented code, as requested, in MediaCCCLiveStreamExtractor. Also use the EMPTY_STRING constant instead of "" in the same class.
    Reformat some code in the class.

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/extractors/MediaCCCLiveStreamExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/extractors/MediaCCCLiveStreamExtractor.java
@@ -3,51 +3,51 @@ package org.schabi.newpipe.extractor.services.media_ccc.extractors;
 import com.grack.nanojson.JsonArray;
 import com.grack.nanojson.JsonObject;
 import org.schabi.newpipe.extractor.MediaFormat;
-import org.schabi.newpipe.extractor.MetaInfo;
 import org.schabi.newpipe.extractor.StreamingService;
 import org.schabi.newpipe.extractor.downloader.Downloader;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.LinkHandler;
-import org.schabi.newpipe.extractor.localization.DateWrapper;
 import org.schabi.newpipe.extractor.stream.*;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 import static org.schabi.newpipe.extractor.stream.AudioStream.UNKNOWN_BITRATE;
+import static org.schabi.newpipe.extractor.utils.Utils.EMPTY_STRING;
 
 public class MediaCCCLiveStreamExtractor extends StreamExtractor {
-    private JsonArray doc = null;
     private JsonObject conference = null;
     private String group = "";
     private JsonObject room = null;
 
-    public MediaCCCLiveStreamExtractor(StreamingService service, LinkHandler linkHandler) {
+    public MediaCCCLiveStreamExtractor(final StreamingService service,
+                                       final LinkHandler linkHandler) {
         super(service, linkHandler);
     }
 
     @Override
-    public void onFetchPage(@Nonnull Downloader downloader) throws IOException, ExtractionException {
-        doc = MediaCCCParsingHelper.getLiveStreams(downloader, getExtractorLocalization());
-        // find correct room
+    public void onFetchPage(@Nonnull final Downloader downloader)
+            throws IOException, ExtractionException {
+        final JsonArray doc = MediaCCCParsingHelper.getLiveStreams(downloader,
+                getExtractorLocalization());
+        // Find correct room
         for (int c = 0; c < doc.size(); c++) {
-            final JsonObject conference = doc.getObject(c);
-            final JsonArray groups = conference.getArray("groups");
+            final JsonObject conferenceObject = doc.getObject(c);
+            final JsonArray groups = conferenceObject.getArray("groups");
             for (int g = 0; g < groups.size(); g++) {
-                final String group = groups.getObject(g).getString("group");
+                final String groupObject = groups.getObject(g).getString("group");
                 final JsonArray rooms = groups.getObject(g).getArray("rooms");
                 for (int r = 0; r < rooms.size(); r++) {
-                    final JsonObject room = rooms.getObject(r);
-                    if (getId().equals(conference.getString("slug") + "/" + room.getString("slug"))) {
-                        this.conference = conference;
-                        this.group = group;
-                        this.room = room;
+                    final JsonObject roomObject = rooms.getObject(r);
+                    if (getId().equals(conferenceObject.getString("slug") + "/"
+                            + roomObject.getString("slug"))) {
+                        this.conference = conferenceObject;
+                        this.group = groupObject;
+                        this.room = roomObject;
                         return;
                     }
                 }
@@ -91,22 +91,25 @@ public class MediaCCCLiveStreamExtractor extends StreamExtractor {
         return conference.getString("conference");
     }
 
+    /**
+     * Get the URL of the first HLS stream found.
+     * <p>
+     * There can be several HLS streams, so the URL of the first found is returned by this method.
+     * <br>
+     * You can find the other video HLS streams
+     * </p>
+     */
     @Nonnull
     @Override
     public String getHlsUrl() {
-        // TODO: There are multiple HLS streams.
-        //       Make getHlsUrl() and getDashMpdUrl() return lists of VideoStreams, so the user can choose a resolution.
         for (int s = 0; s < room.getArray("streams").size(); s++) {
             final JsonObject stream = room.getArray("streams").getObject(s);
-            if (stream.getString("type").equals("video")) {
-                //final String resolution = stream.getArray("videoSize").getInt(0) + "x"
-                //        + stream.getArray("videoSize").getInt(1);
-                if (stream.has("hls")) {
-                    return stream.getObject("urls").getObject("hls").getString("url");
-                }
+            final JsonObject urls = stream.getObject("urls");
+            if (urls.has("hls")) {
+                return urls.getObject("hls").getString("url");
             }
         }
-        return "";
+        return EMPTY_STRING;
     }
 
     @Override
@@ -116,9 +119,27 @@ public class MediaCCCLiveStreamExtractor extends StreamExtractor {
             final JsonObject stream = room.getArray("streams").getObject(s);
             if (stream.getString("type").equals("audio")) {
                 for (final String type : stream.getObject("urls").keySet()) {
-                    final JsonObject url = stream.getObject("urls").getObject(type);
-                    audioStreams.add(new AudioStream(url.getString("tech"), url.getString("url"),
-                            MediaFormat.getFromSuffix(type), UNKNOWN_BITRATE));
+                    final JsonObject urlObject = stream.getObject("urls").getObject(type);
+                    // The DASH manifest will be extracted with getDashMpdUrl
+                    if (!type.equals("dash")) {
+                        if (type.equals("hls")) {
+                            audioStreams.add(new AudioStream(urlObject.getString("tech"),
+                                    urlObject.getString("url"),
+                                    true,
+                                    // We don't know with the type string what media format will
+                                    // have HLS streams.
+                                    // However, the tech string may contain some information about
+                                    // the media format used.
+                                    null,
+                                    DeliveryMethod.HLS,
+                                    UNKNOWN_BITRATE));
+                        } else {
+                            audioStreams.add(new AudioStream(urlObject.getString("tech"),
+                                    urlObject.getString("url"),
+                                    MediaFormat.getFromSuffix(type),
+                                    UNKNOWN_BITRATE));
+                        }
+                    }
                 }
             }
         }
@@ -134,12 +155,29 @@ public class MediaCCCLiveStreamExtractor extends StreamExtractor {
                 final String resolution = stream.getArray("videoSize").getInt(0) + "x"
                         + stream.getArray("videoSize").getInt(1);
                 for (final String type : stream.getObject("urls").keySet()) {
-                    if (!type.equals("hls")) {
-                        final JsonObject url = stream.getObject("urls").getObject(type);
-                        videoStreams.add(new VideoStream(url.getString("tech"),
-                                url.getString("url"),
-                                MediaFormat.getFromSuffix(type),
-                                resolution, false));
+                    final JsonObject urlObject = stream.getObject("urls").getObject(type);
+                    // The DASH manifest will be extracted with getDashMpdUrl
+                    if (!type.equals("dash")) {
+                        if (type.equals("hls")) {
+                            videoStreams.add(new VideoStream(urlObject.getString("tech"),
+                                    urlObject.getString("url"),
+                                    true,
+                                    // We don't know with the type string what type will have HLS
+                                    // streams.
+                                    // However, the tech string may contain some information about
+                                    // the media format used.
+                                    null,
+                                    DeliveryMethod.HLS,
+                                    resolution,
+                                    false,
+                                    null));
+                        } else {
+                            videoStreams.add(new VideoStream(urlObject.getString("tech"),
+                                    urlObject.getString("url"),
+                                    MediaFormat.getFromSuffix(type),
+                                    resolution,
+                                    false));
+                        }
                     }
                 }
             }
@@ -149,7 +187,7 @@ public class MediaCCCLiveStreamExtractor extends StreamExtractor {
 
     @Override
     public List<VideoStream> getVideoOnlyStreams() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
