664cff3f364068158da889fceed135e29bb33850
AioiLight <info@aioilight.space>
2022-01-04 03:41:56 +0900

    Use data-api-data

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/niconico/extractors/NiconicoStreamExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/niconico/extractors/NiconicoStreamExtractor.java
@@ -1,5 +1,9 @@
 package org.schabi.newpipe.extractor.services.niconico.extractors;
 
+import com.grack.nanojson.JsonObject;
+import com.grack.nanojson.JsonParser;
+import com.grack.nanojson.JsonParserException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.schabi.newpipe.extractor.InfoItem;
@@ -9,10 +13,12 @@ import org.schabi.newpipe.extractor.MediaFormat;
 import org.schabi.newpipe.extractor.MetaInfo;
 import org.schabi.newpipe.extractor.StreamingService;
 import org.schabi.newpipe.extractor.downloader.Downloader;
+import org.schabi.newpipe.extractor.downloader.Response;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.LinkHandler;
 import org.schabi.newpipe.extractor.localization.DateWrapper;
+import org.schabi.newpipe.extractor.localization.Localization;
 import org.schabi.newpipe.extractor.stream.AudioStream;
 import org.schabi.newpipe.extractor.stream.Description;
 import org.schabi.newpipe.extractor.stream.StreamExtractor;
@@ -34,8 +40,7 @@ import javax.annotation.Nullable;
 public class NiconicoStreamExtractor extends StreamExtractor {
     private static final String THUMB_INFO_URL = "https://ext.nicovideo.jp/api/getthumbinfo/";
     private static final String UPLOADER_URL = "https://www.nicovideo.jp/user/";
-    private Document thumbInfo;
-    private Document watch;
+    private JsonObject watch;
 
     public NiconicoStreamExtractor(StreamingService service, LinkHandler linkHandler) {
         super(service, linkHandler);
@@ -43,37 +48,37 @@ public class NiconicoStreamExtractor extends StreamExtractor {
 
     @Override
     public long getViewCount() throws ParsingException {
-        return Long.parseLong(thumbInfo.select("view_counter").text());
+        return watch.getObject("video").getObject("count").getLong("view");
     }
 
     @Nonnull
     @Override
     public Description getDescription() throws ParsingException {
-        return new Description(thumbInfo.select("description").text(), 3);
+        return new Description(watch.getObject("video").getString("description"), 1);
     }
 
     @Nonnull
     @Override
     public String getThumbnailUrl() throws ParsingException {
-        return thumbInfo.select("thumbnail_url").text();
+        return watch.getObject("video").getObject("thumbnail").getString("url");
     }
 
     @Nonnull
     @Override
     public String getUploaderUrl() throws ParsingException {
-        return UPLOADER_URL + thumbInfo.select("user_id").text();
+        return UPLOADER_URL + watch.getObject("owner").getLong("id");
     }
 
     @Nonnull
     @Override
     public String getUploaderName() throws ParsingException {
-        return thumbInfo.select("user_nickname").text();
+        return watch.getObject("owner").getString("nickname");
     }
 
     @Nonnull
     @Override
     public String getUploaderAvatarUrl() throws ParsingException {
-        return thumbInfo.select("user_icon_url").text();
+        return watch.getObject("owner").getString("iconUrl");
     }
 
     @Override
@@ -104,14 +109,22 @@ public class NiconicoStreamExtractor extends StreamExtractor {
 
     @Override
     public void onFetchPage(@Nonnull Downloader downloader) throws IOException, ExtractionException {
-        final String info = downloader.get(THUMB_INFO_URL + getLinkHandler().getId()).responseBody();
-        thumbInfo = Jsoup.parse(info);
-        watch = Jsoup.parse(downloader.get(getLinkHandler().getUrl()).responseBody());
+        // generally, Niconico uses Japanese, but some videos have multiple language texts.
+        // Use ja-JP locale to get original information of video.
+        final Localization locale = Localization.fromLocalizationCode("ja-JP");
+        final Response response = downloader.get(getLinkHandler().getUrl(), null, locale);
+        final Document page = Jsoup.parse(response.responseBody());
+        try {
+            watch = JsonParser.object().from(
+                    page.getElementById("js-initial-watch-data").attr("data-api-data"));
+        } catch (JsonParserException e) {
+           throw new ExtractionException("could not extract watching page");
+        }
     }
 
     @Nonnull
     @Override
     public String getName() throws ParsingException {
-        return thumbInfo.select("title").text();
+        return watch.getObject("video").getString("title");
     }
 }
