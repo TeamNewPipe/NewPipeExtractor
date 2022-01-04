660630bd6237cee5ebcb156fb6a674f07b6b0b18
AioiLight <info@aioilight.space>
2022-01-04 22:24:10 +0900

    Add some tests (shorted link, and smartphone page)

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/niconico/NiconicoStreamExtractorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/niconico/NiconicoStreamExtractorTest.java
@@ -18,6 +18,7 @@ import javax.annotation.Nullable;
 public class NiconicoStreamExtractorTest {
 
     public static class SM9 extends DefaultStreamExtractorTest {
+        // the first Niconico video
         private static StreamExtractor extractor;
         private final static String URL = "https://www.nicovideo.jp/watch/sm9";
 
@@ -94,7 +95,7 @@ public class NiconicoStreamExtractorTest {
 
         @Override
         public boolean expectedHasRelatedItems() {
-            return  false;
+            return false;
         }
 
         @Override
@@ -127,6 +128,368 @@ public class NiconicoStreamExtractorTest {
         @Override
         public long expectedLikeCountAtLeast() {
             return 11097;
+        }
+
+        @Override
+        public long expectedDislikeCountAtLeast() {
+            // Niconico does not have Dislike button
+            return -1;
+        }
+    }
+
+    public static class SM70 extends DefaultStreamExtractorTest {
+        // Shorted link (nico.ms)
+        private static StreamExtractor extractor;
+        private final static String SHORTED_URL = "https://nico.ms/sm70";
+        private final static String ACTUAL_URL = "https://www.nicovideo.jp/watch/sm70";
+
+        @BeforeClass
+        public static void setUp() throws Exception {
+            NewPipe.init(DownloaderTestImpl.getInstance());
+            extractor = Niconico.getStreamExtractor(SHORTED_URL);
+            extractor.fetchPage();
+        }
+
+        @Override
+        public StreamExtractor extractor() throws Exception {
+            return extractor;
+        }
+
+        @Override
+        public StreamingService expectedService() throws Exception {
+            return Niconico;
+        }
+
+        @Override
+        public String expectedName() throws Exception {
+            return "エージェント夜を往くverとかちつくちて　アイドルマスター";
+        }
+
+        @Override
+        public String expectedId() throws Exception {
+            return "sm70";
+        }
+
+        @Override
+        public String expectedUrlContains() throws Exception {
+            return ACTUAL_URL;
+        }
+
+        @Override
+        public String expectedOriginalUrlContains() throws Exception {
+            return SHORTED_URL;
+        }
+
+        @Override
+        public StreamType expectedStreamType() {
+            return StreamType.VIDEO_STREAM;
+        }
+
+        @Override
+        public String expectedUploaderName() {
+            return "一般会員";
+        }
+
+        @Override
+        public String expectedUploaderUrl() {
+            return "https://www.nicovideo.jp/user/19242";
+        }
+
+        @Override
+        public boolean expectedHasFrames() {
+            // Niconico does not support video preview with free account.
+            return false;
+        }
+
+        @Override
+        public boolean expectedHasSubtitles() {
+            // Niconico does not support subtitles, but there are uploader's comments.
+            return false;
+        }
+
+        @Override
+        public List<String> expectedDescriptionContains() {
+            final List<String> descs = new ArrayList<>();
+            descs.add("とかちつくちて");
+            return descs;
+        }
+
+        @Override
+        public boolean expectedHasRelatedItems() {
+            return false;
+        }
+
+        @Override
+        public boolean expectedHasAudioStreams() {
+            return false;
+        }
+
+        @Override
+        public long expectedLength() {
+            return 126;
+        }
+
+        @Override
+        public long expectedViewCountAtLeast() {
+            return 162631;
+        }
+
+        @Nullable
+        @Override
+        public String expectedUploadDate() {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public String expectedTextualUploadDate() {
+            return null;
+        }
+
+        @Override
+        public long expectedLikeCountAtLeast() {
+            return 3446;
+        }
+
+        @Override
+        public long expectedDislikeCountAtLeast() {
+            // Niconico does not have Dislike button
+            return -1;
+        }
+    }
+
+    public static class SM1715919 extends DefaultStreamExtractorTest {
+        // Smartphone page (sp.nicovideo.jp)
+        private static StreamExtractor extractor;
+        private final static String SHORTED_URL = "https://sp.nicovideo.jp/watch/sm1715919";
+        private final static String ACTUAL_URL = "https://www.nicovideo.jp/watch/sm1715919";
+
+        @BeforeClass
+        public static void setUp() throws Exception {
+            NewPipe.init(DownloaderTestImpl.getInstance());
+            extractor = Niconico.getStreamExtractor(SHORTED_URL);
+            extractor.fetchPage();
+        }
+
+        @Override
+        public StreamExtractor extractor() throws Exception {
+            return extractor;
+        }
+
+        @Override
+        public StreamingService expectedService() throws Exception {
+            return Niconico;
+        }
+
+        @Override
+        public String expectedName() throws Exception {
+            return "初音ミク　が　オリジナル曲を歌ってくれたよ「メルト」";
+        }
+
+        @Override
+        public String expectedId() throws Exception {
+            return "sm1715919";
+        }
+
+        @Override
+        public String expectedUrlContains() throws Exception {
+            return ACTUAL_URL;
+        }
+
+        @Override
+        public String expectedOriginalUrlContains() throws Exception {
+            return SHORTED_URL;
+        }
+
+        @Override
+        public StreamType expectedStreamType() {
+            return StreamType.VIDEO_STREAM;
+        }
+
+        @Override
+        public String expectedUploaderName() {
+            return "ryo";
+        }
+
+        @Override
+        public String expectedUploaderUrl() {
+            return "https://www.nicovideo.jp/user/317063";
+        }
+
+        @Override
+        public boolean expectedHasFrames() {
+            // Niconico does not support video preview with free account.
+            return false;
+        }
+
+        @Override
+        public boolean expectedHasSubtitles() {
+            // Niconico does not support subtitles, but there are uploader's comments.
+            return false;
+        }
+
+        @Override
+        public List<String> expectedDescriptionContains() {
+            final List<String> descs = new ArrayList<>();
+            descs.add("恋と戦争においてはあらゆる戦術が許される");
+            return descs;
+        }
+
+        @Override
+        public boolean expectedHasRelatedItems() {
+            return false;
+        }
+
+        @Override
+        public boolean expectedHasAudioStreams() {
+            return false;
+        }
+
+        @Override
+        public long expectedLength() {
+            return 256;
+        }
+
+        @Override
+        public long expectedViewCountAtLeast() {
+            return 14791020;
+        }
+
+        @Nullable
+        @Override
+        public String expectedUploadDate() {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public String expectedTextualUploadDate() {
+            return null;
+        }
+
+        @Override
+        public long expectedLikeCountAtLeast() {
+            return 6745;
+        }
+
+        @Override
+        public long expectedDislikeCountAtLeast() {
+            // Niconico does not have Dislike button
+            return -1;
+        }
+    }
+
+    public static class SM37761910 extends DefaultStreamExtractorTest {
+        // New specification video (max. size 1.5GB)
+        private static StreamExtractor extractor;
+        private final static String URL = "https://www.nicovideo.jp/watch/sm37761910";
+
+        @BeforeClass
+        public static void setUp() throws Exception {
+            NewPipe.init(DownloaderTestImpl.getInstance());
+            extractor = Niconico.getStreamExtractor(URL);
+            extractor.fetchPage();
+        }
+
+        @Override
+        public StreamExtractor extractor() throws Exception {
+            return extractor;
+        }
+
+        @Override
+        public StreamingService expectedService() throws Exception {
+            return Niconico;
+        }
+
+        @Override
+        public String expectedName() throws Exception {
+            return "【Ado】うっせぇわ";
+        }
+
+        @Override
+        public String expectedId() throws Exception {
+            return "sm37761910";
+        }
+
+        @Override
+        public String expectedUrlContains() throws Exception {
+            return URL;
+        }
+
+        @Override
+        public String expectedOriginalUrlContains() throws Exception {
+            return URL;
+        }
+
+        @Override
+        public StreamType expectedStreamType() {
+            return StreamType.VIDEO_STREAM;
+        }
+
+        @Override
+        public String expectedUploaderName() {
+            return "Ado";
+        }
+
+        @Override
+        public String expectedUploaderUrl() {
+            return "https://www.nicovideo.jp/user/39170211";
+        }
+
+        @Override
+        public boolean expectedHasFrames() {
+            // Niconico does not support video preview with free account.
+            return false;
+        }
+
+        @Override
+        public boolean expectedHasSubtitles() {
+            // Niconico does not support subtitles, but there are uploader's comments.
+            return false;
+        }
+
+        @Override
+        public List<String> expectedDescriptionContains() {
+            final List<String> descs = new ArrayList<>();
+            descs.add("お前が一番うるさいよ");
+            return descs;
+        }
+
+        @Override
+        public boolean expectedHasRelatedItems() {
+            return false;
+        }
+
+        @Override
+        public boolean expectedHasAudioStreams() {
+            return false;
+        }
+
+        @Override
+        public long expectedLength() {
+            return 202;
+        }
+
+        @Override
+        public long expectedViewCountAtLeast() {
+            return 5592634;
+        }
+
+        @Nullable
+        @Override
+        public String expectedUploadDate() {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public String expectedTextualUploadDate() {
+            return null;
+        }
+
+        @Override
+        public long expectedLikeCountAtLeast() {
+            return 8996;
         }
 
         @Override
