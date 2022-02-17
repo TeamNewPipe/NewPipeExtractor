13f7900816b25d2b286994b4d8dc7749e5712ef7
Stypox <stypox@pm.me>
2022-02-17 18:25:38 +0100

    [YouTube] Add test for genre mix

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/YoutubeMixPlaylistExtractorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/YoutubeMixPlaylistExtractorTest.java
@@ -403,4 +403,94 @@ public class YoutubeMixPlaylistExtractorTest {
             assertEquals(PlaylistInfo.PlaylistType.MIX_CHANNEL, extractor.getPlaylistType());
         }
     }
+
+    public static class GenreMix {
+        private static final String VIDEO_ID = "kINJeTNFbpg";
+        private static final String MIX_TITLE = "Mix – Electronic music";
+
+        @BeforeAll
+        public static void setUp() throws Exception {
+            YoutubeParsingHelper.resetClientVersionAndKey();
+            YoutubeParsingHelper.setNumberGenerator(new Random(1));
+            NewPipe.init(new DownloaderFactory().getDownloader(RESOURCE_PATH + "genreMix"));
+            dummyCookie.put(YoutubeMixPlaylistExtractor.COOKIE_NAME, "whatever");
+            extractor = (YoutubeMixPlaylistExtractor) YouTube
+                    .getPlaylistExtractor("https://www.youtube.com/watch?v=" + VIDEO_ID
+                            + "&list=RDGMEMYH9CUrFO7CfLJpaD7UR85w");
+            extractor.fetchPage();
+        }
+
+        @Test
+        void getServiceId() {
+            assertEquals(YouTube.getServiceId(), extractor.getServiceId());
+        }
+
+        @Test
+        void getName() throws Exception {
+            assertEquals(MIX_TITLE, extractor.getName());
+        }
+
+        @Test
+        void getThumbnailUrl() throws Exception {
+            final String thumbnailUrl = extractor.getThumbnailUrl();
+            assertIsSecureUrl(thumbnailUrl);
+            ExtractorAsserts.assertContains("yt", thumbnailUrl);
+            ExtractorAsserts.assertContains(VIDEO_ID, thumbnailUrl);
+        }
+
+        @Test
+        void getInitialPage() throws Exception {
+            final InfoItemsPage<StreamInfoItem> streams = extractor.getInitialPage();
+            assertFalse(streams.getItems().isEmpty());
+            assertTrue(streams.hasNextPage());
+        }
+
+        @Test
+        void getPage() throws Exception {
+            final byte[] body = JsonWriter.string(prepareDesktopJsonBuilder(
+                    NewPipe.getPreferredLocalization(), NewPipe.getPreferredContentCountry())
+                    .value("videoId", VIDEO_ID)
+                    .value("playlistId", "RD" + VIDEO_ID)
+                    .value("params", "OAE%3D")
+                    .done())
+                    .getBytes(StandardCharsets.UTF_8);
+
+            final InfoItemsPage<StreamInfoItem> streams = extractor.getPage(new Page(
+                    YOUTUBEI_V1_URL + "next?key=" + getKey(), null, null, dummyCookie, body));
+            assertFalse(streams.getItems().isEmpty());
+            assertTrue(streams.hasNextPage());
+        }
+
+        @Test
+        void getContinuations() throws Exception {
+            InfoItemsPage<StreamInfoItem> streams = extractor.getInitialPage();
+            final Set<String> urls = new HashSet<>();
+
+            // Should work infinitely, but for testing purposes only 3 times
+            for (int i = 0; i < 3; i++) {
+                assertTrue(streams.hasNextPage());
+                assertFalse(streams.getItems().isEmpty());
+
+                for (final StreamInfoItem item : streams.getItems()) {
+                    // TODO Duplicates are appearing
+                    // assertFalse(urls.contains(item.getUrl()));
+                    urls.add(item.getUrl());
+                }
+
+                streams = extractor.getPage(streams.getNextPage());
+            }
+            assertTrue(streams.hasNextPage());
+            assertFalse(streams.getItems().isEmpty());
+        }
+
+        @Test
+        void getStreamCount() {
+            assertEquals(ListExtractor.ITEM_COUNT_INFINITE, extractor.getStreamCount());
+        }
+
+        @Test
+        void getPlaylistType() throws ParsingException {
+            assertEquals(PlaylistInfo.PlaylistType.MIX_GENRE, extractor.getPlaylistType());
+        }
+    }
 }
