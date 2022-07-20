2f3ee8a3f2adf1cfd0f99542054c5becb8fa9d98
AudricV <74829229+AudricV@users.noreply.github.com>
2022-07-20 23:28:06 +0200

    Replace avatar and thumbnail URLs attributes and methods to List<Image> in InfoItems

--- extractor/src/main/java/org/schabi/newpipe/extractor/InfoItem.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/InfoItem.java
@@ -1,33 +1,36 @@
-package org.schabi.newpipe.extractor;
-
 /*
  * Created by Christian Schabesberger on 11.02.17.
  *
  * Copyright (C) Christian Schabesberger 2017 <chris.schabesberger@mailbox.org>
- * InfoItem.java is part of NewPipe.
+ * InfoItem.java is part of NewPipe Extractor.
  *
- * NewPipe is free software: you can redistribute it and/or modify
+ * NewPipe Extractor is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * NewPipe is distributed in the hope that it will be useful,
+ * NewPipe Extractor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with NewPipe.  If not, see <http://www.gnu.org/licenses/>.
+ * along with NewPipe Extractor.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+package org.schabi.newpipe.extractor;
+
+import javax.annotation.Nonnull;
 import java.io.Serializable;
+import java.util.List;
 
 public abstract class InfoItem implements Serializable {
     private final InfoType infoType;
     private final int serviceId;
     private final String url;
     private final String name;
-    private String thumbnailUrl;
+    @Nonnull
+    private List<Image> thumbnails = List.of();
 
     public InfoItem(final InfoType infoType,
                     final int serviceId,
@@ -55,12 +58,13 @@ public abstract class InfoItem implements Serializable {
         return name;
     }
 
-    public void setThumbnailUrl(final String thumbnailUrl) {
-        this.thumbnailUrl = thumbnailUrl;
+    public void setThumbnails(@Nonnull final List<Image> thumbnails) {
+        this.thumbnails = thumbnails;
     }
 
-    public String getThumbnailUrl() {
-        return thumbnailUrl;
+    @Nonnull
+    public List<Image> getThumbnails() {
+        return thumbnails;
     }
 
     @Override
