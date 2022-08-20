295df11940cba660dc44774dbe4b7d3b8ecd6c92
ThetaDev <t.testboy@gmail.com>
2022-08-20 14:27:59 +0200

    clean up JavaScriptExtractorTest

--- extractor/src/test/java/org/schabi/newpipe/extractor/utils/JavaScriptExtractorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/utils/JavaScriptExtractorTest.java
@@ -4,20 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.utils.jsextractor.JavaScriptExtractor;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavaScriptExtractorTest
 {
     @Test
-    public void testJsExtractor() throws ParsingException, IOException {
-        final String evil_js = "Wka=function(d){var x = [/,,/,913,/(,)}/g,\"abcdef}\\\"\",];var y = 10/2/1;return x[1][y];}//some={}random-padding+;";
-        // String evil_js = "Wka=function(d){var x = [\"abc\",\"abcdef}\\\"\",];var y = 10/2/1;return x[1][y];}//some={}random-padding+;";
-
-        // Div only
-        // String evil_js = "Wka=function(d){var y = 10/2/1;return y;}//some={}random-padding+;";
-        final String result = JavaScriptExtractor.matchToClosingBrace(evil_js, "Wka=function");
+    public void testJsExtractor() throws ParsingException {
+        final String src = "Wka=function(d){var x = [/,,/,913,/(,)}/g,\"abcdef}\\\"\",];var y = 10/2/1;return x[1][y];}//some={}random-padding+;";
+        final String result = JavaScriptExtractor.matchToClosingBrace(src, "Wka=function");
         assertEquals("(d){var x = [/,,/,913,/(,)}/g,\"abcdef}\\\"\",];var y = 10/2/1;return x[1][y];}", result);
     }
 }
