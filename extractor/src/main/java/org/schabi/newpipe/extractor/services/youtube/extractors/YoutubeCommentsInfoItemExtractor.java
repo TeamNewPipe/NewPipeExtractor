c4f6a69dbcbdad86207a25b1cb4e6ba8a4558237
xz-dev <xiangzhedev@gmail.com>
2022-10-07 16:31:44 +0800

    Throw reply count parsing error in Youtube

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeCommentsInfoItemExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeCommentsInfoItemExtractor.java
@@ -1,6 +1,5 @@
 package org.schabi.newpipe.extractor.services.youtube.extractors;
 
-import static org.schabi.newpipe.extractor.comments.CommentsInfoItem.UNKNOWN_REPLY_COUNT;
 import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.getTextFromObject;
 
 import com.grack.nanojson.JsonArray;
@@ -251,11 +250,7 @@ public class YoutubeCommentsInfoItemExtractor implements CommentsInfoItemExtract
 
     @Override
     public int getReplyCount() throws ParsingException {
-        try {
-            return JsonUtils.getNumber(json, "comment.commentRenderer.replyCount").intValue();
-        } catch (final Exception e) {
-            return UNKNOWN_REPLY_COUNT;
-        }
+        return JsonUtils.getNumber(json, "comment.commentRenderer.replyCount").intValue();
     }
 
     @Override
