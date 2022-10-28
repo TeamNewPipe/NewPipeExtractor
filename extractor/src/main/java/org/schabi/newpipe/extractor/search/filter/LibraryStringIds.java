1d552c4533d2eca9546be51b9cfb0c18c86df9bf
evermind <evermind@tuxfamily.org>
2022-10-29 00:39:01 +0200

    searchfilters: an enum class which with strings ids that will be used within the searchfilters
    The class LibraryStringIds is a enum.
    This enum's identify a message. The name of the enum constants
    reflect the actual message. This message has to be transformed by the
    library user into a meaningful string.

---
+++ extractor/src/main/java/org/schabi/newpipe/extractor/search/filter/LibraryStringIds.java
@@ -0,0 +1,90 @@
+// Created by evermind-zz 2022, licensed GNU GPL version 3 or later
+
+package org.schabi.newpipe.extractor.search.filter;
+
+/**
+ * 'strings' are used within the library, especially in classes derived
+ * from {@link BaseSearchFilters}.
+ * <p>
+ * We use an enum to identify the message. The name of the enum constants
+ * reflect the actual message. This message has to be transformed by the
+ * library user into a meaningful string.
+ *
+ * <b>NOTE:</b> Behind each listed enum the actual string is in a comment that the
+ * library user should translate/use.
+ */
+public enum LibraryStringIds {
+    SEARCH_FILTERS_10_30_MIN, /* 10-30 min */
+    SEARCH_FILTERS_2_10_MIN, /* 2-10 min */
+    SEARCH_FILTERS_360, /* 360° */
+    SEARCH_FILTERS_3D, /* 3d */
+    SEARCH_FILTERS_4_20_MIN, /* 4-20 min */
+    SEARCH_FILTERS_4K, /* 4k */
+    SEARCH_FILTERS_ADDED, /* Added */
+    SEARCH_FILTERS_ALBUMS, /* albums */
+    SEARCH_FILTERS_ALL, /* all */
+    SEARCH_FILTERS_ANY_TIME, /* Any time */
+    SEARCH_FILTERS_ARTISTS, /* artists */
+    SEARCH_FILTERS_ARTISTS_AND_LABELS, /* artists & labels */
+    SEARCH_FILTERS_ASCENDING, /* Ascending */
+    SEARCH_FILTERS_CCOMMONS, /* Ccommons */
+    SEARCH_FILTERS_CHANNELS, /* Channels */
+    SEARCH_FILTERS_CONFERENCES, /* conferences */
+    SEARCH_FILTERS_CREATION_DATE, /* Creation date */
+    SEARCH_FILTERS_DATE, /* Date */
+    SEARCH_FILTERS_DURATION, /* Duration */
+    SEARCH_FILTERS_EVENTS, /* events */
+    SEARCH_FILTERS_FEATURES, /* Features */
+    SEARCH_FILTERS_GREATER_30_MIN, /* > 30 min */
+    SEARCH_FILTERS_HD, /* HD */
+    SEARCH_FILTERS_HDR, /* Hdr */
+    SEARCH_FILTERS_KIND, /* Kind */
+    SEARCH_FILTERS_LAST_30_DAYS, /* Last 30 days */
+    SEARCH_FILTERS_LAST_7_DAYS, /* Last 7 days */
+    SEARCH_FILTERS_LAST_HOUR, /* Last hour */
+    SEARCH_FILTERS_LAST_YEAR, /* Last year */
+    SEARCH_FILTERS_LENGTH, /* Length */
+    SEARCH_FILTERS_LESS_2_MIN, /* < 2 min */
+    SEARCH_FILTERS_LICENSE, /* License */
+    SEARCH_FILTERS_LIKES, /* Likes */
+    SEARCH_FILTERS_LIVE, /* Live */
+    SEARCH_FILTERS_LOCATION, /* Location */
+    SEARCH_FILTERS_LONG_GREATER_10_MIN, /* Long (> 10 min) */
+    SEARCH_FILTERS_MEDIUM_4_10_MIN, /* Medium (4-10 min) */
+    SEARCH_FILTERS_NAME, /* Name */
+    SEARCH_FILTERS_NO, /* No */
+    SEARCH_FILTERS_OVER_20_MIN, /* Over 20 min */
+    SEARCH_FILTERS_PAST_DAY, /* Past day */
+    SEARCH_FILTERS_PAST_HOUR, /* Past hour */
+    SEARCH_FILTERS_PAST_MONTH, /* Past month */
+    SEARCH_FILTERS_PAST_WEEK, /* Past week */
+    SEARCH_FILTERS_PAST_YEAR, /* Past year */
+    SEARCH_FILTERS_PLAYLISTS, /* Playlists */
+    SEARCH_FILTERS_PUBLISH_DATE, /* Publish date */
+    SEARCH_FILTERS_PUBLISHED, /* Published */
+    SEARCH_FILTERS_RATING, /* Rating */
+    SEARCH_FILTERS_RELEVANCE, /* Relevance */
+    SEARCH_FILTERS_SENSITIVE, /* Sensitive */
+    SEARCH_FILTERS_SEPIASEARCH, /* SepiaSearch */
+    SEARCH_FILTERS_SHORT_LESS_4_MIN, /* Short (< 4 min) */
+    SEARCH_FILTERS_SONGS, /* songs */
+    SEARCH_FILTERS_SORT_BY, /* Sort by */
+    SEARCH_FILTERS_SORT_ORDER, /* Sort order */
+    SEARCH_FILTERS_SUBTITLES, /* Subtitles */
+    SEARCH_FILTERS_TO_MODIFY_COMMERCIALLY, /* To modify commercially */
+    SEARCH_FILTERS_TODAY, /* Today */
+    SEARCH_FILTERS_TRACKS, /* tracks */
+    SEARCH_FILTERS_UNDER_4_MIN, /* Under 4 min */
+    SEARCH_FILTERS_UPLOAD_DATE, /* Upload Date */
+    SEARCH_FILTERS_USERS, /* users */
+    SEARCH_FILTERS_VIDEOS, /* Videos */
+    SEARCH_FILTERS_VIEWS, /* Views */
+    SEARCH_FILTERS_VOD_VIDEOS, /* VOD videos */
+    SEARCH_FILTERS_VR180, /* VR180 */
+    SEARCH_FILTERS_THIS_MONTH, /* This month */
+    SEARCH_FILTERS_THIS_WEEK, /* This week */
+    SEARCH_FILTERS_THIS_YEAR, /* This year */
+    SEARCH_FILTERS_YES, /* Yes */
+    SEARCH_FILTERS_YOUTUBE_MUSIC, /* YouTube Music */
+    SEARCH_FILTERS_PURCHASED, /* Purchased */
+}
