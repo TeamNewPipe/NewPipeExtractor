294ffab4905404574fa580e9b014405af1a9c3ef
Stypox <stypox@pm.me>
2023-04-14 16:51:17 +0200

    Add documentation for ReadyChannelTabLLH

--- extractor/src/main/java/org/schabi/newpipe/extractor/linkhandler/ReadyChannelTabListLinkHandler.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/linkhandler/ReadyChannelTabListLinkHandler.java
@@ -5,6 +5,21 @@ import org.schabi.newpipe.extractor.channel.ChannelTabExtractor;
 
 import java.util.Collections;
 
+/**
+ * Can be used as a {@link ListLinkHandler} to be returned from {@link
+ * org.schabi.newpipe.extractor.channel.ChannelInfo#getTabs()} when a specific tab's data has
+ * already been fetched. This class allows passing a builder for a {@link ChannelTabExtractor} that
+ * can hold references to variables.
+ *
+ * Note: a service that wishes to use this class in one of its {@link
+ * org.schabi.newpipe.extractor.channel.ChannelExtractor}s must also add the following snippet of
+ * code in the service's {@link StreamingService#getChannelTabExtractor(ListLinkHandler)}:
+ * <pre>
+ * if (linkHandler instanceof ReadyChannelTabListLinkHandler) {
+ *     return ((ReadyChannelTabListLinkHandler) linkHandler).getChannelTabExtractor(this);
+ * }
+ * </pre>
+ */
 public class ReadyChannelTabListLinkHandler extends ListLinkHandler {
 
     public interface ChannelTabExtractorBuilder {
