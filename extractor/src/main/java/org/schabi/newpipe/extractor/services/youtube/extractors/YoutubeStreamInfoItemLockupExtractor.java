a18e98adbb9036737c8089f401b9d6f946868460
litetex <40789489+litetex@users.noreply.github.com>
2025-07-08 21:14:46 +0200

    Remove copyright from original file

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeStreamInfoItemLockupExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeStreamInfoItemLockupExtractor.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (C) 2016 Christian Schabesberger <chris.schabesberger@mailbox.org>
- * YoutubeStreamInfoItemExtractor.java is part of NewPipe Extractor.
- *
- * NewPipe Extractor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * NewPipe Extractor is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with NewPipe Extractor.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package org.schabi.newpipe.extractor.services.youtube.extractors;
 
 import static org.schabi.newpipe.extractor.utils.Utils.isNullOrEmpty;
