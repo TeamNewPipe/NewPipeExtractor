8bbb7e984cf8f33d002794442acc31dc4c7f692e
litetex <40789489+litetex@users.noreply.github.com>
2025-07-13 21:13:31 +0200

    Modify ``SoundcloudChannelTabExtractorTest`` so that it compiles
    Also removed irrelevant methods
    
    See https://github.com/TeamNewPipe/NewPipeExtractor/pull/1308

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/soundcloud/SoundcloudChannelTabExtractorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/soundcloud/SoundcloudChannelTabExtractorTest.java
@@ -13,12 +13,10 @@ import org.schabi.newpipe.extractor.services.DefaultListExtractorTest;
 class SoundcloudChannelTabExtractorTest {
 
     static class Tracks extends DefaultListExtractorTest<ChannelTabExtractor> {
-
         @Override
         protected ChannelTabExtractor createExtractor() throws Exception {
             return SoundCloud.getChannelTabExtractorFromId("10494998", ChannelTabs.TRACKS);
         }
-
 
         @Override public StreamingService expectedService() throws Exception { return SoundCloud; }
         @Override public String expectedName() throws Exception { return ChannelTabs.TRACKS; }
@@ -26,7 +24,6 @@ class SoundcloudChannelTabExtractorTest {
         @Override public String expectedUrlContains() throws Exception { return "https://soundcloud.com/liluzivert/tracks"; }
         @Override public String expectedOriginalUrlContains() throws Exception { return "https://soundcloud.com/liluzivert/tracks"; }
         @Override public InfoItem.InfoType expectedInfoItemType() { return InfoItem.InfoType.STREAM; }
-        @Override public boolean expectedHasMoreItems() { return true; }
 
         @Test
         void testGetPageInNewExtractor() throws Exception {
@@ -48,7 +45,6 @@ class SoundcloudChannelTabExtractorTest {
         @Override public String expectedUrlContains() throws Exception { return "https://soundcloud.com/neffexmusic/sets"; }
         @Override public String expectedOriginalUrlContains() throws Exception { return "https://soundcloud.com/neffexmusic/sets"; }
         @Override public InfoItem.InfoType expectedInfoItemType() { return InfoItem.InfoType.PLAYLIST; }
-        @Override public boolean expectedHasMoreItems() { return true; }
     }
 
     static class Albums extends DefaultListExtractorTest<ChannelTabExtractor> {
@@ -63,62 +59,33 @@ class SoundcloudChannelTabExtractorTest {
         @Override public String expectedUrlContains() throws Exception { return "https://soundcloud.com/bigsean-1/albums"; }
         @Override public String expectedOriginalUrlContains() throws Exception { return "https://soundcloud.com/bigsean-1/albums"; }
         @Override public InfoItem.InfoType expectedInfoItemType() { return InfoItem.InfoType.PLAYLIST; }
-        @Override public boolean expectedHasMoreItems() { return true; }
     }
 
     static class LikesOnlyTracks extends DefaultListExtractorTest<ChannelTabExtractor> {
-        private static SoundcloudChannelTabExtractor extractor;
-
-        @BeforeAll
-        static void setUp() throws IOException, ExtractionException {
-            NewPipe.init(DownloaderTestImpl.getInstance());
-            extractor = (SoundcloudChannelTabExtractor) SoundCloud
-                    .getChannelTabExtractorFromId("30854092", ChannelTabs.LIKES);
-            extractor.fetchPage();
+        @Override
+        protected ChannelTabExtractor createExtractor() throws Exception {
+            return SoundCloud.getChannelTabExtractorFromId("30854092", ChannelTabs.LIKES);
         }
 
-        @Override public ChannelTabExtractor extractor() throws Exception { return extractor; }
         @Override public StreamingService expectedService() throws Exception { return SoundCloud; }
         @Override public String expectedName() throws Exception { return ChannelTabs.LIKES; }
         @Override public String expectedId() throws Exception { return "30854092"; }
         @Override public String expectedUrlContains() throws Exception { return "https://soundcloud.com/lubenitza/likes"; }
         @Override public String expectedOriginalUrlContains() throws Exception { return "https://soundcloud.com/lubenitza/likes"; }
         @Override public InfoItem.InfoType expectedInfoItemType() { return InfoItem.InfoType.STREAM; }
-        @Override public boolean expectedHasMoreItems() { return true; }
-
-        @Test
-        void testGetPageInNewExtractor() throws Exception {
-            final ChannelTabExtractor newTabExtractor =
-                    SoundCloud.getChannelTabExtractorFromId("30854092", ChannelTabs.LIKES);
-            defaultTestGetPageInNewExtractor(extractor, newTabExtractor);
-        }
     }
 
     static class LikesOnlyPlaylists extends DefaultListExtractorTest<ChannelTabExtractor> {
-        private static SoundcloudChannelTabExtractor extractor;
-
-        @BeforeAll
-        static void setUp() throws IOException, ExtractionException {
-            NewPipe.init(DownloaderTestImpl.getInstance());
-            extractor = (SoundcloudChannelTabExtractor) SoundCloud
-                    .getChannelTabExtractorFromId("1280839267", ChannelTabs.LIKES);
-            extractor.fetchPage();
+        @Override
+        protected ChannelTabExtractor createExtractor() throws Exception {
+            return SoundCloud.getChannelTabExtractorFromId("1280839267", ChannelTabs.LIKES);
         }
 
-        @Override public ChannelTabExtractor extractor() throws Exception { return extractor; }
         @Override public StreamingService expectedService() throws Exception { return SoundCloud; }
         @Override public String expectedName() throws Exception { return ChannelTabs.LIKES; }
         @Override public String expectedId() throws Exception { return "1280839267"; }
         @Override public String expectedUrlContains() throws Exception { return "https://soundcloud.com/soreen-735855039/likes"; }
         @Override public String expectedOriginalUrlContains() throws Exception { return "https://soundcloud.com/soreen-735855039/likes"; }
         @Override public InfoItem.InfoType expectedInfoItemType() { return InfoItem.InfoType.PLAYLIST; }
-        @Override public boolean expectedHasMoreItems() { return true; }
-
-        @Test
-        void testGetPageInNewExtractor() throws Exception {
-            final ChannelTabExtractor newTabExtractor =
-                    SoundCloud.getChannelTabExtractorFromId("1280839267", ChannelTabs.LIKES);
-            defaultTestGetPageInNewExtractor(extractor, newTabExtractor);
-        }
     }
 }
