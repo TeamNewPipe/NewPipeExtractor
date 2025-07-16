9a7487215c5883fb16662abac37f858c9a457000
litetex <40789489+litetex@users.noreply.github.com>
2025-07-16 19:38:02 +0200

    Update docs
    Co-Authored-By: Stypox <stypox@pm.me>

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/DefaultSimpleUntypedExtractorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/DefaultSimpleUntypedExtractorTest.java
@@ -2,10 +2,23 @@ package org.schabi.newpipe.extractor.services;
 
 import org.schabi.newpipe.extractor.InitNewPipeTest;
 
+
+/**
+ * Represents a common abstraction of a test class where an extractor is set up and various
+ * tests for it are executed.
+ * <p/>
+ * Please see {@link DefaultSimpleExtractorTest} for an implementation that is exactly typed to
+ * {@link org.schabi.newpipe.extractor.Extractor}
+ */
 public abstract class DefaultSimpleUntypedExtractorTest<T> implements InitNewPipeTest {
 
     private T extractor;
 
+    /**
+     * This should create an extractor, save it to variable
+     * {@link DefaultSimpleUntypedExtractorTest#extractor},
+     * and then set up the extractor by making it fetch network resources if applicable
+     */
     protected void initExtractor() throws Exception {
         extractor = createExtractor();
         fetchExtractor(extractor);
