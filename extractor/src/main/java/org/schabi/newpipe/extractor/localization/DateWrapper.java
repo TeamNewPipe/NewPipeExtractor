e26cc84f9985bab0542d13ba21c70b02ea287cd7
Isira Seneviratne <isirasen96@gmail.com>
2025-10-10 08:15:46 +0530

    Added documentation for parsing methods

--- extractor/src/main/java/org/schabi/newpipe/extractor/localization/DateWrapper.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/localization/DateWrapper.java
@@ -3,6 +3,7 @@ package org.schabi.newpipe.extractor.localization;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.Serializable;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -90,6 +91,15 @@ public class DateWrapper implements Serializable {
                 + '}';
     }
 
+    /**
+     * Parses a date string that matches the ISO-8601 {@link OffsetDateTime} pattern, e.g.
+     * "2011-12-03T10:15:30+01:00".
+     *
+     * @param date The date string
+     * @return a non-approximate {@link DateWrapper}, or null if the string is null
+     * @throws ParsingException if the string does not match the expected format
+     */
+    @Nullable
     public static DateWrapper fromOffsetDateTime(final String date) throws ParsingException {
         try {
             return date != null ? new DateWrapper(OffsetDateTime.parse(date)) : null;
@@ -98,6 +108,15 @@ public class DateWrapper implements Serializable {
         }
     }
 
+    /**
+     * Parses a date string that matches the ISO-8601 {@link Instant} pattern, e.g.
+     * "2011-12-03T10:15:30Z".
+     *
+     * @param date The date string
+     * @return a non-approximate {@link DateWrapper}, or null if the string is null
+     * @throws ParsingException if the string does not match the expected format
+     */
+    @Nullable
     public static DateWrapper fromInstant(final String date) throws ParsingException {
         try {
             return date != null ? new DateWrapper(Instant.parse(date)) : null;
