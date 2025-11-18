25b04f2dbb7e1537353ce9e25c10df7c735783a1
AbsurdlyLongUsername <22662897+absurdlylongusername@users.noreply.github.com>
2025-11-18 05:50:07 +0000

    Fix null exception in tests Add string formatting to logger Add tests for logger formatting

--- extractor/src/main/java/org/schabi/newpipe/extractor/utils/ExtractorLogger.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/utils/ExtractorLogger.java
@@ -4,72 +4,176 @@ public final class ExtractorLogger {
 
     private ExtractorLogger() { }
 
-    private static Logger logger = new EmptyLogger();
+    private static final Logger EMPTY_LOGGER = new EmptyLogger();
+    private static volatile Logger logger = EMPTY_LOGGER;
 
     public static void setLogger(final Logger customLogger) {
-        logger = customLogger;
+        logger = customLogger != null ? customLogger : EMPTY_LOGGER;
     }
 
+    public enum Level { DEBUG, WARN, ERROR }
+
+    @SuppressWarnings("checkstyle:NeedBraces")
+    private static void log(final Level level,
+                            final String tag,
+                            final String message,
+                            final Throwable t) {
+        if (logger == EMPTY_LOGGER) return;
+        switch (level) {
+            case DEBUG:
+                if (t == null) {
+                    logger.debug(tag, message);
+                } else {
+                    logger.debug(tag, message, t);
+                }
+                break;
+            case WARN:
+                if (t == null) {
+                    logger.warn(tag, message);
+                } else {
+                    logger.warn(tag, message, t);
+                }
+                break;
+            case ERROR:
+                if (t == null) {
+                    logger.error(tag, message);
+                } else {
+                    logger.error(tag, message, t);
+                }
+                break;
+        }
+    }
+
+    @SuppressWarnings("checkstyle:NeedBraces")
+    private static void logFormat(final Level level,
+                                  final String tag,
+                                  final Throwable t,
+                                  final String template,
+                                  final Object... args) {
+        if (logger == EMPTY_LOGGER) return;
+        log(level, tag, format(template, args), t);
+    }
+
+    // DEBUG
     public static void d(final String tag, final String msg) {
-        logger.debug(tag, msg);
+        log(Level.DEBUG, tag, msg, null);
     }
 
     public static void d(final String tag, final String msg, final Throwable t) {
-        logger.debug(tag, msg, t);
+        log(Level.DEBUG, tag, msg, t);
     }
 
+    public static void d(final String tag, final String template, final Object... args) {
+        logFormat(Level.DEBUG, tag, null, template, args);
+    }
+
+    public static void d(final String tag,
+                         final Throwable t,
+                         final String template,
+                         final Object... args) {
+        logFormat(Level.DEBUG, tag, t, template, args);
+    }
+
+    // WARN
     public static void w(final String tag, final String msg) {
-        logger.warn(tag, msg);
+        log(Level.WARN, tag, msg, null);
     }
 
     public static void w(final String tag, final String msg, final Throwable t) {
-        logger.warn(tag, msg, t);
+        log(Level.WARN, tag, msg, t);
     }
 
+    public static void w(final String tag, final String template, final Object... args) {
+        logFormat(Level.WARN, tag, null, template, args);
+    }
+
+    public static void w(final String tag,
+                         final Throwable t,
+                         final String template,
+                         final Object... args) {
+        logFormat(Level.WARN, tag, t, template, args);
+    }
+
+    // ERROR
     public static void e(final String tag, final String msg) {
-        logger.error(tag, msg);
+        log(Level.ERROR, tag, msg, null);
     }
 
     public static void e(final String tag, final String msg, final Throwable t) {
-        logger.error(tag, msg, t);
+        log(Level.ERROR, tag, msg, t);
     }
 
+    public static void e(final String tag, final String template, final Object... args) {
+        logFormat(Level.ERROR, tag, null, template, args);
+    }
 
-    private static final class EmptyLogger implements Logger {
-        public void debug(final String tag, final String msg) { }
-
-        @Override
-        public void debug(final String tag, final String msg, final Throwable throwable) { }
-
-        public void warn(final String tag, final String msg) { }
-
-        @Override
-        public void warn(final String tag, final String msg, final Throwable t) { }
-
-        public void error(final String tag, final String msg) { }
-
-        public void error(final String tag, final String msg, final Throwable t) { }
+    public static void e(final String tag,
+                         final Throwable t,
+                         final String template,
+                         final Object... args) {
+        logFormat(Level.ERROR, tag, t, template, args);
     }
 
     /**
-     *  Logger that prints to stdout
+     * Simple string format method for easier logger in the form of
+     * {@code ExtractorLogger.d("Hello my name {Name} {}", name, surname)}
+     * @param template The template string to format
+     * @param args Arguments to replace identifiers with in {@code template}
+     * @return Formatted string with arguments replaced
      */
+    private static String format(final String template, final Object... args) {
+        if (template == null || args == null || args.length == 0) {
+            return template;
+        }
+        final var out = new StringBuilder(template.length() + Math.min(32, 16 * args.length));
+        int cursorIndex = 0;
+        int argIndex = 0;
+        final int n = template.length();
+        while (cursorIndex < n) {
+            // Find first/next open brace
+            final int openBraceIndex = template.indexOf('{', cursorIndex);
+            if (openBraceIndex < 0) {
+                // If none found then there's no more arguments to replace
+                out.append(template, cursorIndex, n); break;
+            }
+
+            // Find matching closing brace
+            final int close = template.indexOf('}', openBraceIndex + 1);
+            if (close < 0) {
+                // If none found then there's no more arguments to replace
+                out.append(template, cursorIndex, n); break;
+            }
+            out.append(template, cursorIndex, openBraceIndex);
+            out.append(argIndex < args.length
+                ? String.valueOf(args[argIndex++])
+                : template.substring(openBraceIndex, close + 1));
+            cursorIndex = close + 1;
+        }
+        return out.toString();
+    }
+
+    private static final class EmptyLogger implements Logger {
+        public void debug(final String tag, final String msg) { }
+        public void debug(final String tag, final String msg, final Throwable throwable) { }
+        public void warn(final String tag, final String msg) { }
+        public void warn(final String tag, final String msg, final Throwable t) { }
+        public void error(final String tag, final String msg) { }
+        public void error(final String tag, final String msg, final Throwable t) { }
+    }
+
     public static final class ConsoleLogger implements Logger {
         public void debug(final String tag, final String msg) {
-        System.out.println("[DEBUG][" + tag + "] " + msg);
+            System.out.println("[DEBUG][" + tag + "] " + msg);
         }
 
-        @Override
         public void debug(final String tag, final String msg, final Throwable throwable) {
             debug(tag, msg);
             throwable.printStackTrace(System.err);
         }
-
         public void warn(final String tag, final String msg) {
             System.out.println("[WARN ][" + tag + "] " + msg);
         }
 
-        @Override
         public void warn(final String tag, final String msg, final Throwable t) {
             warn(tag, msg);
             t.printStackTrace(System.err);
