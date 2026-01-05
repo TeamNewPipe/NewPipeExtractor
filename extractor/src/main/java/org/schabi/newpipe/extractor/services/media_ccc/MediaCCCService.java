TeamNewPipe/NewPipeExtractor

commit 1e8474b22d387d280b06f174f7ff10ef8baa180b

[PeerTube] Add tabs support for accounts and video channels
Support of channels and videos has been added for accounts and support of
videos and playlists has been added for video channels.

The following changes have been also done:
- collectStreamsFrom method in PeertubeParsingHelper has been renamed to
collectItemsFrom;
- PeertubeChannelInfoItemExtractor.getStreamCount method has been fixed due to
ChannelExtractor's new inheritance;
- the declaration of the UnsupportedOperationException exception thrown has
been added to the service's LinkHandlers;
- a channel tab LinkHandlerFactory has been added,
PeertubeChannelTabLinkHandlerFactory;
- all service's LinkHandlers are now using properly the singleton pattern.

Co-authored-by: ThetaDev <t.testboy@gmail.com>
Co-authored-by: Stypox <stypox@pm.me>

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/MediaCCCService.java
@@ -6,6 +6,7 @@ import static java.util.Arrays.asList;
 
 import org.schabi.newpipe.extractor.StreamingService;
 import org.schabi.newpipe.extractor.channel.ChannelExtractor;
+import org.schabi.newpipe.extractor.channel.tabs.ChannelTabExtractor;
 import org.schabi.newpipe.extractor.comments.CommentsExtractor;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
 import org.schabi.newpipe.extractor.kiosk.KioskList;
@@ -13,6 +14,7 @@ import org.schabi.newpipe.extractor.linkhandler.LinkHandler;
 import org.schabi.newpipe.extractor.linkhandler.LinkHandlerFactory;
 import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
 import org.schabi.newpipe.extractor.linkhandler.ListLinkHandlerFactory;
+import org.schabi.newpipe.extractor.linkhandler.ReadyChannelTabListLinkHandler;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandler;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
 import org.schabi.newpipe.extractor.playlist.PlaylistExtractor;
@@ -27,8 +29,6 @@ import org.schabi.newpipe.extractor.services.media_ccc.extractors.MediaCCCSearch
 import org.schabi.newpipe.extractor.services.media_ccc.extractors.MediaCCCStreamExtractor;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCConferenceLinkHandlerFactory;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCConferencesListLinkHandlerFactory;
-import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCLiveListLinkHandlerFactory;
-import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCRecentListLinkHandlerFactory;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCSearchQueryHandlerFactory;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCStreamLinkHandlerFactory;
 import org.schabi.newpipe.extractor.stream.StreamExtractor;
@@ -47,12 +47,17 @@ public class MediaCCCService extends StreamingService {
 
     @Override
     public LinkHandlerFactory getStreamLHFactory() {
-        return new MediaCCCStreamLinkHandlerFactory();
+        return MediaCCCStreamLinkHandlerFactory.getInstance();
     }
 
     @Override
     public ListLinkHandlerFactory getChannelLHFactory() {
-        return new MediaCCCConferenceLinkHandlerFactory();
+        return MediaCCCConferenceLinkHandlerFactory.getInstance();
+    }
+
+    @Override
+    public ListLinkHandlerFactory getChannelTabLHFactory() {
+        return null;
     }
 
     @Override
@@ -62,7 +67,7 @@ public class MediaCCCService extends StreamingService {
 
     @Override
     public SearchQueryHandlerFactory getSearchQHFactory() {
-        return new MediaCCCSearchQueryHandlerFactory();
+        return MediaCCCSearchQueryHandlerFactory.getInstance();
     }
 
     @Override
@@ -79,6 +84,22 @@ public class MediaCCCService extends StreamingService {
     }
 
     @Override
+    public ChannelTabExtractor getChannelTabExtractor(final ListLinkHandler linkHandler) {
+        if (linkHandler instanceof ReadyChannelTabListLinkHandler) {
+            return ((ReadyChannelTabListLinkHandler) linkHandler).getChannelTabExtractor(this);
+        }
+
+        /*
+        Channel tab extractors are only supported in conferences and should only come from a
+        ReadyChannelTabListLinkHandler instance with a ChannelTabExtractorBuilder instance of the
+        conferences extractor
+
+        If that's not the case, return null in this case, so no channel tabs support
+        */
+        return null;
+    }
+
+    @Override
     public PlaylistExtractor getPlaylistExtractor(final ListLinkHandler linkHandler) {
         return null;
     }
@@ -91,36 +112,37 @@ public class MediaCCCService extends StreamingService {
     @Override
     public KioskList getKioskList() throws ExtractionException {
         final KioskList list = new KioskList(this);
+        final ListLinkHandlerFactory h = MediaCCCConferencesListLinkHandlerFactory.getInstance();
 
         // add kiosks here e.g.:
         try {
             list.addKioskEntry(
                     (streamingService, url, kioskId) -> new MediaCCCConferenceKiosk(
                             MediaCCCService.this,
-                            new MediaCCCConferencesListLinkHandlerFactory().fromUrl(url),
+                            h.fromUrl(url),
                             kioskId
                     ),
-                    new MediaCCCConferencesListLinkHandlerFactory(),
+                    h,
                     MediaCCCConferenceKiosk.KIOSK_ID
             );
 
             list.addKioskEntry(
                     (streamingService, url, kioskId) -> new MediaCCCRecentKiosk(
                             MediaCCCService.this,
-                            new MediaCCCRecentListLinkHandlerFactory().fromUrl(url),
+                            h.fromUrl(url),
                             kioskId
                     ),
-                    new MediaCCCRecentListLinkHandlerFactory(),
+                    h,
                     MediaCCCRecentKiosk.KIOSK_ID
             );
 
             list.addKioskEntry(
                     (streamingService, url, kioskId) -> new MediaCCCLiveStreamKiosk(
                             MediaCCCService.this,
-                            new MediaCCCLiveListLinkHandlerFactory().fromUrl(url),
+                            h.fromUrl(url),
                             kioskId
                     ),
-                    new MediaCCCLiveListLinkHandlerFactory(),
+                    h,
                     MediaCCCLiveStreamKiosk.KIOSK_ID
             );
 

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/extractors/MediaCCCConferenceExtractor.java
@@ -1,23 +1,29 @@
 package org.schabi.newpipe.extractor.services.media_ccc.extractors;
 
-import com.grack.nanojson.JsonArray;
 import com.grack.nanojson.JsonObject;
 import com.grack.nanojson.JsonParser;
 import com.grack.nanojson.JsonParserException;
+
+import org.schabi.newpipe.extractor.InfoItem;
+import org.schabi.newpipe.extractor.ListExtractor;
+import org.schabi.newpipe.extractor.MultiInfoItemsCollector;
 import org.schabi.newpipe.extractor.Page;
 import org.schabi.newpipe.extractor.StreamingService;
 import org.schabi.newpipe.extractor.channel.ChannelExtractor;
+import org.schabi.newpipe.extractor.channel.tabs.ChannelTabExtractor;
+import org.schabi.newpipe.extractor.channel.tabs.ChannelTabs;
 import org.schabi.newpipe.extractor.downloader.Downloader;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
+import org.schabi.newpipe.extractor.linkhandler.ReadyChannelTabListLinkHandler;
 import org.schabi.newpipe.extractor.services.media_ccc.extractors.infoItems.MediaCCCStreamInfoItemExtractor;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCConferenceLinkHandlerFactory;
-import org.schabi.newpipe.extractor.stream.StreamInfoItem;
-import org.schabi.newpipe.extractor.stream.StreamInfoItemsCollector;
+
+import java.io.IOException;
+import java.util.List;
 
 import javax.annotation.Nonnull;
-import java.io.IOException;
 
 public class MediaCCCConferenceExtractor extends ChannelExtractor {
     private JsonObject conferenceData;
@@ -74,18 +80,9 @@ public class MediaCCCConferenceExtractor extends ChannelExtractor {
 
     @Nonnull
     @Override
-    public InfoItemsPage<StreamInfoItem> getInitialPage() {
-        final StreamInfoItemsCollector collector = new StreamInfoItemsCollector(getServiceId());
-        final JsonArray events = conferenceData.getArray("events");
-        for (int i = 0; i < events.size(); i++) {
-            collector.commit(new MediaCCCStreamInfoItemExtractor(events.getObject(i)));
-        }
-        return new InfoItemsPage<>(collector, null);
-    }
-
-    @Override
-    public InfoItemsPage<StreamInfoItem> getPage(final Page page) {
-        return InfoItemsPage.emptyPage();
+    public List<ListLinkHandler> getTabs() throws ParsingException {
+        return List.of(new ReadyChannelTabListLinkHandler(getUrl(), getId(),
+                ChannelTabs.VIDEOS, new VideosTabExtractorBuilder(conferenceData)));
     }
 
     @Override
@@ -104,5 +101,56 @@ public class MediaCCCConferenceExtractor extends ChannelExtractor {
     @Override
     public String getName() throws ParsingException {
         return conferenceData.getString("title");
+    }
+
+    private static final class VideosTabExtractorBuilder
+            implements ReadyChannelTabListLinkHandler.ChannelTabExtractorBuilder {
+
+        private final JsonObject conferenceData;
+
+        VideosTabExtractorBuilder(final JsonObject conferenceData) {
+            this.conferenceData = conferenceData;
+        }
+
+        @Nonnull
+        @Override
+        public ChannelTabExtractor build(@Nonnull final StreamingService service,
+                                         @Nonnull final ListLinkHandler linkHandler) {
+            return new VideosChannelTabExtractor(service, linkHandler, conferenceData);
+        }
+    }
+
+    private static final class VideosChannelTabExtractor extends ChannelTabExtractor {
+        private final JsonObject conferenceData;
+
+        VideosChannelTabExtractor(final StreamingService service,
+                                  final ListLinkHandler linkHandler,
+                                  final JsonObject conferenceData) {
+            super(service, linkHandler);
+            this.conferenceData = conferenceData;
+        }
+
+        @Override
+        public void onFetchPage(@Nonnull final Downloader downloader) {
+            // Nothing to do here, as data was already fetched
+        }
+
+        @Nonnull
+        @Override
+        public ListExtractor.InfoItemsPage<InfoItem> getInitialPage() {
+            final MultiInfoItemsCollector collector =
+                    new MultiInfoItemsCollector(getServiceId());
+            conferenceData.getArray("events")
+                    .stream()
+                    .filter(JsonObject.class::isInstance)
+                    .map(JsonObject.class::cast)
+                    .forEach(event -> collector.commit(new MediaCCCStreamInfoItemExtractor(event)));
+            return new InfoItemsPage<>(collector, null);
+        }
+
+        @Override
+        public InfoItemsPage<InfoItem> getPage(final Page page) {
+            return InfoItemsPage.emptyPage();
+        }
     }
 }

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/extractors/MediaCCCRecentKioskExtractor.java
@@ -65,7 +65,7 @@ public class MediaCCCRecentKioskExtractor implements StreamInfoItemExtractor {
 
     @Override
     public String getUploaderUrl() throws ParsingException {
-        return new MediaCCCConferenceLinkHandlerFactory()
+        return MediaCCCConferenceLinkHandlerFactory.getInstance()
                 .fromUrl(event.getString("conference_url")) // API URL
                 .getUrl(); // web URL
     }

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/extractors/MediaCCCSearchExtractor.java
@@ -38,7 +38,8 @@ public class MediaCCCSearchExtractor extends SearchExtractor {
         super(service, linkHandler);
         try {
             conferenceKiosk = new MediaCCCConferenceKiosk(service,
-                    new MediaCCCConferencesListLinkHandlerFactory().fromId("conferences"),
+                    MediaCCCConferencesListLinkHandlerFactory.getInstance()
+                            .fromId("conferences"),
                     "conferences");
         } catch (final Exception e) {
             e.printStackTrace();

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/linkHandler/MediaCCCConferenceLinkHandlerFactory.java
@@ -6,7 +6,11 @@ import org.schabi.newpipe.extractor.utils.Parser;
 
 import java.util.List;
 
-public class MediaCCCConferenceLinkHandlerFactory extends ListLinkHandlerFactory {
+public final class MediaCCCConferenceLinkHandlerFactory extends ListLinkHandlerFactory {
+
+    private static final MediaCCCConferenceLinkHandlerFactory INSTANCE
+            = new MediaCCCConferenceLinkHandlerFactory();
+
     public static final String CONFERENCE_API_ENDPOINT
             = "https://api.media.ccc.de/public/conferences/";
     public static final String CONFERENCE_PATH = "https://media.ccc.de/c/";
@@ -14,15 +18,23 @@ public class MediaCCCConferenceLinkHandlerFactory extends ListLinkHandlerFactory
             = "(?:(?:(?:api\\.)?media\\.ccc\\.de/public/conferences/)"
             + "|(?:media\\.ccc\\.de/[bc]/))([^/?&#]*)";
 
+    private MediaCCCConferenceLinkHandlerFactory() {
+    }
+
+    public static MediaCCCConferenceLinkHandlerFactory getInstance() {
+        return INSTANCE;
+    }
+
     @Override
     public String getUrl(final String id,
                          final List<String> contentFilter,
-                         final String sortFilter) throws ParsingException {
+                         final String sortFilter)
+            throws ParsingException, UnsupportedOperationException {
         return CONFERENCE_PATH + id;
     }
 
     @Override
-    public String getId(final String url) throws ParsingException {
+    public String getId(final String url) throws ParsingException, UnsupportedOperationException {
         return Parser.matchGroup1(ID_PATTERN, url);
     }
 

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/linkHandler/MediaCCCConferencesListLinkHandlerFactory.java
@@ -5,15 +5,28 @@ import org.schabi.newpipe.extractor.linkhandler.ListLinkHandlerFactory;
 
 import java.util.List;
 
-public class MediaCCCConferencesListLinkHandlerFactory extends ListLinkHandlerFactory {
+public final class MediaCCCConferencesListLinkHandlerFactory extends ListLinkHandlerFactory {
+
+    private static final MediaCCCConferencesListLinkHandlerFactory INSTANCE =
+            new MediaCCCConferencesListLinkHandlerFactory();
+
+    private MediaCCCConferencesListLinkHandlerFactory() {
+    }
+
+    public static MediaCCCConferencesListLinkHandlerFactory getInstance() {
+        return INSTANCE;
+    }
+
     @Override
-    public String getId(final String url) throws ParsingException {
+    public String getId(final String url) throws ParsingException, UnsupportedOperationException {
         return "conferences";
     }
 
     @Override
-    public String getUrl(final String id, final List<String> contentFilter,
-                         final String sortFilter) throws ParsingException {
+    public String getUrl(final String id,
+                         final List<String> contentFilter,
+                         final String sortFilter)
+            throws ParsingException, UnsupportedOperationException {
         return "https://media.ccc.de/public/conferences";
     }
 

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/linkHandler/MediaCCCLiveListLinkHandlerFactory.java
@@ -6,11 +6,22 @@ import org.schabi.newpipe.extractor.linkhandler.ListLinkHandlerFactory;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class MediaCCCLiveListLinkHandlerFactory extends ListLinkHandlerFactory {
+public final class MediaCCCLiveListLinkHandlerFactory extends ListLinkHandlerFactory {
+
+    private static final MediaCCCLiveListLinkHandlerFactory INSTANCE =
+            new MediaCCCLiveListLinkHandlerFactory();
+
     private static final String STREAM_PATTERN = "^(?:https?://)?media\\.ccc\\.de/live$";
 
+    private MediaCCCLiveListLinkHandlerFactory() {
+    }
+
+    public static MediaCCCLiveListLinkHandlerFactory getInstance() {
+        return INSTANCE;
+    }
+
     @Override
-    public String getId(final String url) throws ParsingException {
+    public String getId(final String url) throws ParsingException, UnsupportedOperationException {
         return "live";
     }
 
@@ -22,7 +33,8 @@ public class MediaCCCLiveListLinkHandlerFactory extends ListLinkHandlerFactory {
     @Override
     public String getUrl(final String id,
                          final List<String> contentFilter,
-                         final String sortFilter) throws ParsingException {
+                         final String sortFilter)
+            throws ParsingException, UnsupportedOperationException {
         // FIXME: wrong URL; should be https://streaming.media.ccc.de/{conference_slug}/{room_slug}
         return "https://media.ccc.de/live";
     }

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/linkHandler/MediaCCCLiveStreamLinkHandlerFactory.java
@@ -1,32 +1,0 @@
-package org.schabi.newpipe.extractor.services.media_ccc.linkHandler;
-
-import org.schabi.newpipe.extractor.exceptions.ParsingException;
-import org.schabi.newpipe.extractor.linkhandler.LinkHandlerFactory;
-import org.schabi.newpipe.extractor.utils.Parser;
-
-public class MediaCCCLiveStreamLinkHandlerFactory extends LinkHandlerFactory {
-    public static final String VIDEO_API_ENDPOINT = "https://api.media.ccc.de/public/events/";
-    private static final String VIDEO_PATH = "https://streaming.media.ccc.de/v/";
-    private static final String ID_PATTERN
-            = "(?:(?:(?:api\\.)?media\\.ccc\\.de/public/events/)"
-            + "|(?:media\\.ccc\\.de/v/))([^/?&#]*)";
-
-    @Override
-    public String getId(final String url) throws ParsingException {
-        return Parser.matchGroup1(ID_PATTERN, url);
-    }
-
-    @Override
-    public String getUrl(final String id) throws ParsingException {
-        return VIDEO_PATH + id;
-    }
-
-    @Override
-    public boolean onAcceptUrl(final String url) {
-        try {
-            return getId(url) != null;
-        } catch (final ParsingException e) {
-            return false;
-        }
-    }
-}

commit a1e5e9387103a52da9d348fdf59e5aa007fa8cc8

Fix regression where all ListLinkHandlerFactories of media.ccc.de were replaces with one
Regression was introduced in 1e8474b22d387d280b06f174f7ff10ef8baa180b

extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/MediaCCCService.java
@@ -30,6 +30,8 @@ import org.schabi.newpipe.extractor.services.media_ccc.extractors.MediaCCCSearch
 import org.schabi.newpipe.extractor.services.media_ccc.extractors.MediaCCCStreamExtractor;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCConferenceLinkHandlerFactory;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCConferencesListLinkHandlerFactory;
+import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCLiveListLinkHandlerFactory;
+import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCRecentListLinkHandlerFactory;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCSearchQueryHandlerFactory;
 import org.schabi.newpipe.extractor.services.media_ccc.linkHandler.MediaCCCStreamLinkHandlerFactory;
 import org.schabi.newpipe.extractor.stream.StreamExtractor;
@@ -111,37 +113,39 @@ public class MediaCCCService extends StreamingService {
     @Override
     public KioskList getKioskList() throws ExtractionException {
         final KioskList list = new KioskList(this);
-        final ListLinkHandlerFactory h = MediaCCCConferencesListLinkHandlerFactory.getInstance();
+        final var conferencesLHF = MediaCCCConferencesListLinkHandlerFactory.getInstance();
+        final var recentLHF = MediaCCCRecentListLinkHandlerFactory.getInstance();
+        final var liveLHF = MediaCCCLiveListLinkHandlerFactory.getInstance();
 
         // add kiosks here e.g.:
         try {
             list.addKioskEntry(
                     (streamingService, url, kioskId) -> new MediaCCCConferenceKiosk(
                             MediaCCCService.this,
-                            h.fromUrl(url),
+                            conferencesLHF.fromUrl(url),
                             kioskId
                     ),
-                    h,
+                    conferencesLHF,
                     MediaCCCConferenceKiosk.KIOSK_ID
             );
 
             list.addKioskEntry(
                     (streamingService, url, kioskId) -> new MediaCCCRecentKiosk(
                             MediaCCCService.this,
-                            h.fromUrl(url),
+                            recentLHF.fromUrl(url),
                             kioskId
                     ),
-                    h,
+                    recentLHF,
                     MediaCCCRecentKiosk.KIOSK_ID
             );
 
             list.addKioskEntry(
                     (streamingService, url, kioskId) -> new MediaCCCLiveStreamKiosk(
                             MediaCCCService.this,
-                            h.fromUrl(url),
+                            liveLHF.fromUrl(url),
                             kioskId
                     ),
-                    h,
+                    liveLHF,
                     MediaCCCLiveStreamKiosk.KIOSK_ID
             );
 