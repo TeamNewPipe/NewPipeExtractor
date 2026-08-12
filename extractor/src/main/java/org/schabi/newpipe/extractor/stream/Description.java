55cd9d7155ae92ec6b06886f86908294b78c25f3
Isira Seneviratne <isirasen96@gmail.com>
2026-08-12 06:12:23 +0530

    Add SPDX header

--- extractor/src/main/java/org/schabi/newpipe/extractor/stream/Description.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/stream/Description.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2026 NewPipe e.V. <https://newpipe-ev.de>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 package org.schabi.newpipe.extractor.stream;
 
 import javax.annotation.Nonnull;
